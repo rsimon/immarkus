d6d2a560fdc60d6f888bfca4cc9c725f9f75890f
Rainer Simon <rainer@rainersimon.io>
2023-09-01 16:20:16 +0200

    Annotation view: save status indicator

--- src/pages/annotate/SaveStatusIndicator/SaveStatusIndicator.tsx
+++ src/pages/annotate/SaveStatusIndicator/SaveStatusIndicator.tsx
@@ -17,7 +17,7 @@ export const SaveStatusIndicator = (props: SaveStatusIndicatorProps) => {
 
   const { status } = props;
 
-  const fadeOut = props.fadeOut || 5000;
+  const fadeOut = props.fadeOut || 1500;
 
   const [opacity, setOpacity] = useState(1);
 
@@ -37,10 +37,10 @@ export const SaveStatusIndicator = (props: SaveStatusIndicatorProps) => {
   }, [status]);
 
   return (
-    <div className="save-status-wrapper" style={{ opacity: 1 }}>
+    <div className="save-status-wrapper" style={{ opacity }}>
       {status === 'idle' ? (
-        <div className="save-status failed text-xs font-semibold">
-          <X size={16} /><span>Error</span>
+        <div className="save-status success text-xs font-semibold">
+          <Check size={16} /><span>Saved</span>
         </div>
       ) : status === 'success' ? (
         <div className="save-status success text-xs font-semibold">
@@ -51,7 +51,7 @@ export const SaveStatusIndicator = (props: SaveStatusIndicatorProps) => {
           <X size={16} /><span>Error</span>
         </div>
       ) : (
-        <div className="save-status text-xs font-semibold">
+        <div className="save-status saving text-xs font-semibold">
           <span>Saving</span>
         </div>
       )}
