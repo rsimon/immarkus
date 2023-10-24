3e17790dce73ba626061ae9741550fc1376e0fc5
Rainer Simon <rainer@rainersimon.io>
2023-10-24 12:06:06 +0200

    Minor bugfix

--- src/pages/annotate/CurrentSelection/CurrentSelectionMetadata/EntitySchemaFields.tsx
+++ src/pages/annotate/CurrentSelection/CurrentSelectionMetadata/EntitySchemaFields.tsx
@@ -28,38 +28,42 @@ export const EntitySchemaFields = (props: EntitySchemaFieldsProps) => {
 
   const { body, entity, safeKeys, formik } = props;
   
-  return (entity.schema || []).map(property => (
-    <div className="mt-2" key={safeKeys.getKey(body, property.name)}>
-      <Label 
-        htmlFor={safeKeys.getKey(body, property.name)}
-        className="text-xs block mb-1 mt-3">
-        {property.name}
-      </Label>
+  return (
+    <>
+      {(entity.schema || []).map(property => (
+        <div className="mt-2" key={safeKeys.getKey(body, property.name)}>
+          <Label 
+            htmlFor={safeKeys.getKey(body, property.name)}
+            className="text-xs block mb-1 mt-3">
+            {property.name}
+          </Label>
 
-      {property.type === 'enum' ? (
-        <Select 
-          value={formik.values[safeKeys.getKey(body, property.name)]}
-          onValueChange={value => 
-            formik.handleChange(safeKeys.getKey(body, property.name))(value)}>
-          
-          <SelectTrigger className="w-full h-8 mt-0.5">
-            <SelectValue />
-          </SelectTrigger>
+          {property.type === 'enum' ? (
+            <Select 
+              value={formik.values[safeKeys.getKey(body, property.name)]}
+              onValueChange={value => 
+                formik.handleChange(safeKeys.getKey(body, property.name))(value)}>
+              
+              <SelectTrigger className="w-full h-8 mt-0.5">
+                <SelectValue />
+              </SelectTrigger>
 
-          <SelectContent>
-            {property.values.map(option => (
-              <SelectItem key={option} value={option}>{option}</SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      ) : (
-        <Input 
-          id={safeKeys.getKey(body, property.name)} 
-          className="h-8 mt-0.5" 
-          value={formik.values[safeKeys.getKey(body, property.name)]} 
-          onChange={formik.handleChange}/>
-      )}
-    </div>
-  ))
+              <SelectContent>
+                {property.values.map(option => (
+                  <SelectItem key={option} value={option}>{option}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          ) : (
+            <Input 
+              id={safeKeys.getKey(body, property.name)} 
+              className="h-8 mt-0.5" 
+              value={formik.values[safeKeys.getKey(body, property.name)]} 
+              onChange={formik.handleChange}/>
+          )}
+        </div>
+      ))}
+    </>
+  )
 
 }
