c288fd2d5257785666c5cd6a7b141e69e0123f9b
Rainer Simon <rainer@rainersimon.io>
2023-10-31 16:04:39 +0100

    Prepared for OSD integration

--- src/pages/annotate/Annotate.tsx
+++ src/pages/annotate/Annotate.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Annotorious, AnnotoriousPlugin, ImageAnnotator, W3CImageFormat } from '@annotorious/react';
+import { Annotorious, AnnotoriousPlugin, ImageAnnotator, OpenSeadragonAnnotator, OpenSeadragonViewer, W3CImageFormat } from '@annotorious/react';
 import { mountExtension as SelectorPack } from '@annotorious/selector-pack';
 import { ChevronLeft } from 'lucide-react';
 import { useStore } from '@/store';
@@ -12,6 +12,9 @@ import { Tool, Toolbar } from './Toolbar';
 
 import './Annotate.css';
 import '@annotorious/react/annotorious-react.css';
+
+// 'Feature toggle' for future use
+const DISPLAY_MODE: 'image' | 'osd' = 'image';
 
 export const Annotate = () => {
 
@@ -43,8 +46,10 @@ export const Annotate = () => {
   return store &&  (
     <div className="page-root page annotate">
       <Annotorious>
-        <main className="bg-muted relative p-0 flex flex-col overflow-hidden">
-          <div>
+        <main 
+          className={DISPLAY_MODE === 'image' ? 
+            'bg-muted relative p-0 flex flex-col overflow-hidden' : 'bg-muted relative p-0'}>
+          <div className="absolute">
             <nav className="p-3 pr-4 bg-white inline-block border-r rounded-br-lg shadow-md relative z-10">
               <ul className="flex text-sm">
                 <li className="inline-block">
@@ -65,17 +70,41 @@ export const Annotate = () => {
           <SavingIndicator status={saveStatus} />
 
           {image && (
-            <div className="flex justify-center items-center z-0 flex-grow pb-12">
-              <ImageAnnotator
-                // @ts-ignore
-                adapter={W3CImageFormat(image.path)}
-                autoSave={true}
-                tool={tool}>
-                <img 
-                  className="select-none"
-                  src={URL.createObjectURL(image.data)}
-                  alt={image.path} />
-              </ImageAnnotator>
+            <div 
+              className={DISPLAY_MODE === 'image' ?
+                'flex justify-center items-center z-0 flex-grow pb-12' :
+                'h-full w-full'}>
+              
+              {DISPLAY_MODE === 'image' ? (
+                <ImageAnnotator
+                  // @ts-ignore
+                  adapter={W3CImageFormat(image.path)}
+                  autoSave={true}
+                  tool={tool}>
+                  <img 
+                    className="select-none"
+                    src={URL.createObjectURL(image.data)}
+                    alt={image.path} />
+                </ImageAnnotator>
+              ) : (
+                <OpenSeadragonAnnotator
+                  adapter={W3CImageFormat(image.path)}
+                  tool={tool}>
+                  <OpenSeadragonViewer
+                    className="osd-container"
+                    options={{
+                      tileSources: {
+                        type: 'image',
+                        url: URL.createObjectURL(image.data)
+                      },
+                      gestureSettingsMouse: {
+                        clickToZoom: false
+                      },
+                      showNavigationControl: false,
+                      crossOriginPolicy: 'Anonymous'
+                    }} />
+                </OpenSeadragonAnnotator>
+              )}
 
               {/* @ts-ignore */}
               <AnnotoriousPlugin
