6b98dbd474827edd3f0a8d2d604ed551a37d733e
Rainer Simon <rainer@rainersimon.io>
2023-11-03 12:50:58 +0100

    More UI design revisions

--- src/pages/annotate/AnnotateMosaic.tsx
+++ src/pages/annotate/AnnotateMosaic.tsx
@@ -1,23 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import * as Tabs from '@radix-ui/react-tabs';
+import { OpenSeadragonViewer } from '@annotorious/react';
 import { Mosaic, MosaicContext, MosaicWindow } from 'react-mosaic-component';
+import { useStore } from '@/store';
 import { Button } from '@/ui/Button';
 import { Separator } from '@/ui/Separator';
 import { 
+  ChevronDown,
   ChevronLeft, 
-  Circle, 
   Image, 
   ImagePlus, 
   MessagesSquare, 
   MousePointerSquare, 
-  Square, 
-  TriangleRight,
-  X
+  Square,
+  X,
+  ZoomIn,
+  ZoomOut
 } from 'lucide-react';
 
 import './AnnotateMosaic.css';
 import 'react-mosaic-component/react-mosaic-component.css';
-
+import { AnnotationList } from './AnnotationList';
+import { StoragePlugin } from './StoragePlugin';
 
 const TITLE_MAP: Record<string, string> = {
   a: 'Left Window',
@@ -28,10 +32,16 @@ const TITLE_MAP: Record<string, string> = {
 
 export const AnnotateMosaic = () => {
 
+  const store = useStore({ redirect: true });
+
+  const params = useParams();
+
+  const image = store?.getImage(params.id!);
+
   return (
     <div className="page annotate h-full w-full">
       <main className="absolute top-0 left-0 h-full right-[340px] flex flex-col">
-        <section className="toolbar border-b p-1.5 flex justify-between text-sm h-[46px]">
+        <section className="toolbar border-b p-2 flex justify-between text-sm h-[46px]">
           <section className="toolbar-left flex gap-1 items-center">
             <div className=" flex items-center">
               <Link className="font-semibold inline" to="/images">
@@ -41,67 +51,95 @@ export const AnnotateMosaic = () => {
               </Link>
 
               <span className="text-xs font-medium mr-2 ml-0.5">
-                RI_R_002_site position.png
+                {image.name}
               </span>
             </div>
 
-            <button className="p-2 flex text-xs rounded-md bg-muted/70 hover:bg-gray-300/50">
-              <ImagePlus className="h-4 w-4 mr-1" /> Add image
-            </button>
+
           </section>
 
           <section className="toolbar-right flex gap-1.5 items-center">
-            <button className="p-2 flex items-center text-xs rounded-md hover:bg-muted">
-              <Square className="w-4 h-4 mr-1 mb-0.5" />
-              Box
-            </button>
-            
-            <button className="p-2 flex items-center text-xs rounded-md hover:bg-muted">
-              <TriangleRight className="w-4 h-4 mr-1.5 -rotate-[10deg] mb-0.5" />
-              Polygon
+            <button className="p-2 flex text-xs rounded-md hover:bg-muted">
+              <ImagePlus className="h-4 w-4 mr-1" /> Add image
             </button>
 
-            <button className="p-2 flex items-center text-xs rounded-md hover:bg-muted">
-              <Circle className="w-4 h-4 mr-1 scale-y-90 mb-0.5" />
-              Ellipse
+            <button className="pl-2.5 py-2 pr-2 flex items-center text-xs rounded-md hover:bg-muted border shadow-sm">
+              <Square className="w-4 h-4 mr-1 mb-0.5" />
+              Rectangle
+              <ChevronDown className="h-3 w-3 ml-1" />
             </button>
+            
+            {/* 
+              <button className="p-2 flex items-center text-xs rounded-md hover:bg-muted">
+                <TriangleRight className="w-4 h-4 mr-1.5 -rotate-[10deg] mb-0.5" />
+                Polygon
+              </button>
+
+              <button className="p-2 flex items-center text-xs rounded-md hover:bg-muted">
+                <Circle className="w-4 h-4 mr-1 scale-y-90 mb-0.5" />
+                Ellipse
+              </button>
+            */}
           </section>
         </section>
 
         <section className="workspace flex-grow bg-muted">
-          <Mosaic
-            renderTile={(id, path) => (
-              <MosaicWindow 
-                path={path}
-                className="text-xs"
-                title={TITLE_MAP[id]}
-                toolbarControls={(
-                  <MosaicContext.Consumer>
-                    {({ mosaicActions}) => (
-                      <Button 
-                        variant="ghost" 
-                        size="icon"
-                        className="h-6 w-6 p-0 rounded-full mr-1 text-muted-foreground hover:text-black"
-                        onClick={() => mosaicActions.remove(path)}>
-                        <X className="h-4 w-4" />
-                      </Button>
-                    )}
-                  </MosaicContext.Consumer>
-                )}>
-               
-              
-               
-              </MosaicWindow>
-            )}
-            initialValue={{
-              direction: 'row',
-              first: 'a',
-              second: {
-                direction: 'column',
-                first: 'b',
-                second: 'c',
-              },
-            }} />
+          {image && (
+            <Mosaic
+              renderTile={(id, path) => (
+                <MosaicWindow 
+                  path={path}
+                  className="text-xs"
+                  title={image.name}
+                  toolbarControls={(
+                    <>
+                      <button>
+                        <ZoomIn className="h-4 w-4 mr-2.5 text-muted-foreground hover:text-black" />
+                      </button>
+
+                      <button>
+                        <ZoomOut className="h-4 w-4 mr-1.5 text-muted-foreground hover:text-black" />
+                      </button>
+
+                      <MosaicContext.Consumer>
+                        {({ mosaicActions}) => (
+                          <Button 
+                            variant="ghost" 
+                            size="icon"
+                            className="h-6 w-6 p-0 rounded-full mr-1 text-muted-foreground hover:text-black"
+                            onClick={() => mosaicActions.remove(path)}>
+                            <X className="h-4 w-4" />
+                          </Button>
+                        )}
+                      </MosaicContext.Consumer>
+                    </>
+                  )}>
+
+                  <OpenSeadragonViewer
+                    className="osd-container"
+                    options={{
+                      tileSources: {
+                        type: 'image',
+                        url: URL.createObjectURL(image.data)
+                      },
+                      gestureSettingsMouse: {
+                        clickToZoom: false
+                      },
+                      showNavigationControl: false,
+                      crossOriginPolicy: 'Anonymous'
+                    }} />
+                </MosaicWindow>
+              )}
+              initialValue={{
+                direction: 'row',
+                first: 'a',
+                second: {
+                  direction: 'column',
+                  first: 'b',
+                  second: 'c',
+                },
+              }} />
+          )}
         </section>
       </main>
 
@@ -110,16 +148,16 @@ export const AnnotateMosaic = () => {
           <section className="toolbar border-b h-[46px] flex items-center">
             <Separator orientation="vertical" className="h-4" />
 
-            <Tabs.List className="flex gap-1.5 py-0.5 px-1">
-              <Tabs.Trigger value="selection" className="p-2 flex text-xs rounded-md hover:bg-muted">
+            <Tabs.List className="flex gap-1.5 py-0.5 px-2">
+              <Tabs.Trigger value="selection" className="p-2 flex text-xs rounded-md bg-black text-white">
                 <MousePointerSquare className="h-4 w-4 mr-1" /> Selection
               </Tabs.Trigger>
 
-              <Tabs.Trigger value="annotation-list" className="p-2 flex text-xs rounded-md hover:bg-muted">
+              <Tabs.Trigger value="annotation-list" className="p-2 flex text-xs rounded-md hover:bg-muted text-muted-foreground">
                 <MessagesSquare className="h-4 w-4 mr-1" /> List
               </Tabs.Trigger>
 
-              <Tabs.Trigger value="image-notes" className="p-2 flex text-xs rounded-md hover:bg-muted">
+              <Tabs.Trigger value="image-notes" className="p-2 flex text-xs rounded-md hover:bg-muted text-muted-foreground">
                 <Image className="h-4 w-4 mr-1" /> Image Notes
               </Tabs.Trigger>
             </Tabs.List>
@@ -127,7 +165,7 @@ export const AnnotateMosaic = () => {
         </Tabs.Root>
 
         <section className="sidebar-content bg-muted/80 flex-grow border-l">
-
+          <AnnotationList />
         </section>
       </aside>
     </div>
