09f6f29e71e945548f37f76d42c3653bb3703ecd
Rainer Simon <rainer@rainersimon.io>
2023-12-19 14:08:07 +0100

    Style tweaks

--- src/components/PropertyFields/ExternalAuthorityField/ExternalAuthoritySelector.tsx
+++ src/components/PropertyFields/ExternalAuthorityField/ExternalAuthoritySelector.tsx
@@ -36,7 +36,8 @@ export const ExternalAuthoritySelector = (props: ExternalAuthoritySelectorProps)
      {authorities.length === 1 ? (
         <button 
           onClick={() => onOpen(authorities[0].name)}
-          className="text-xs flex items-center hover:bg-slate-200 px-1.5 py-0.5 rounded focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0">
+          className="text-xs flex items-center hover:bg-slate-200 px-1.5 py-0.5 rounded font-medium
+            focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0">
           <Search className="h-3.5 w-3.5 mr-1" />
           {props.definition.authorities[0].name}
         </button>
@@ -50,14 +51,14 @@ export const ExternalAuthoritySelector = (props: ExternalAuthoritySelectorProps)
             
             <button 
               onClick={() => onOpen(value)}
-              className="text-xs flex items-center hover:bg-slate-200 px-1.5 py-1 pr-1.5 rounded 
+              className="text-xs flex items-center hover:bg-slate-200 px-1.5 py-1 pr-1.5 rounded font-medium
                 focus:outline-none focus:ring-0 focus:ring-ring focus:ring-offset-0 focus-visible:outline-none 
                 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0">
               <Search className="h-3.5 w-3.5 mr-1" /> <SelectValue className="pr-0" />
             </button>       
 
             <SelectTrigger
-              className="border-none -ml-0.5 pr-1.5 pl-0.5 py-1 bg-transparent hover:bg-slate-200 focus:outline-none focus:ring-0 
+              className="text-black border-none -ml-0.5 pr-1.5 pl-0.5 py-1 bg-transparent hover:bg-slate-200 focus:outline-none focus:ring-0 
                 focus:ring-ring focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-2 
                 focus-visible:ring-ring focus-visible:ring-offset-2"/>
           </div>
@@ -69,7 +70,8 @@ export const ExternalAuthoritySelector = (props: ExternalAuthoritySelectorProps)
             {authorities.map(authority => (
               <SelectItem 
                 key={authority.name} 
-                value={authority.name}>
+                value={authority.name}
+                className="text-xs">
                 {authority.name}
               </SelectItem>
             ))}
