e34f4d6382fe56c76a34a2e6c0bf3a2b5f4dfcb7
Rainer Simon <rainer@rainersimon.io>
2024-01-02 17:00:34 +0100

    Refactoring: Autosuggest component is now re-usable

--- src/components/EntityTypeEditor/Editor/ParentBrowser/ParentBrowser.tsx
+++ src/components/EntityTypeEditor/Editor/ParentBrowser/ParentBrowser.tsx
@@ -1,12 +1,10 @@
-import { Children, useState } from 'react';
-import Autosuggest from 'react-autosuggest';
-import { cn } from '@/ui/utils';
+import { Autosuggest } from '@/ui/Autosuggest';
 import { useStore } from '@/store';
 import { EntityType } from '@/model';
 
 interface ParentBrowserProps {
 
-  id: string;
+  id?: string;
 
   className?: string;
 
@@ -20,52 +18,25 @@ export const ParentBrowser = (props: ParentBrowserProps) => {
 
   const store = useStore();
 
-  const [suggestions, setSuggestions] = useState([]);
+  const getSuggestions = (query: string) => 
+    store.searchEntityTypes(query);
 
-  const onGetSuggestions = ({ value }: { value: string }) =>
-    setSuggestions(store.searchEntityTypes(value));
-
-  const inputClass = cn(
-    'flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-    props.className
-  );
-
-  const containerClass = 
-    'react-autosuggest__suggestions-container absolute mt-1.5 w-full z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg'
-
-  const suggestionClass =
-    'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground';
-
-  const renderSuggestion = (type: EntityType, { isHighlighted }) => (
-    <div className={suggestionClass} data-highlighted={isHighlighted ? 'true' : undefined}>
-      {type.label ? (
-        <>{type.label} <span className="text-muted-foreground/60 ml-1.5">{type.id}</span></>
-      ) : (
-        <>{type.id}</>
-      )}
-    </div>
+  const renderSuggestion = (type: EntityType) => (
+    type.label ? (
+      <>{type.label} <span className="text-muted-foreground/60 ml-1.5">{type.id}</span></>
+    ) : (
+      <>{type.id}</>
+    )
   )
 
   return (
     <Autosuggest 
-      suggestions={suggestions} 
-      onSuggestionsFetchRequested={onGetSuggestions}
-      onSuggestionsClearRequested={() => setSuggestions([])}
-      getSuggestionValue={type => type.id}
-      renderSuggestionsContainer={({ containerProps, children }) => Children.toArray(children).length > 0 ? (
-        <div {...containerProps} className={containerClass}>
-          {children}
-        </div>
-      ) : null}
-      renderSuggestion={renderSuggestion}
-      containerProps={{
-        className: 'relative'
-      }}
-      inputProps={{
-        className: inputClass,
-        value: props.value,
-        onChange: (_, { newValue }) => props.onChange(newValue)
-      }} />
+      id={props.id}
+      className={props.className}
+      value={props.value}
+      onChange={props.onChange}
+      getSuggestions={getSuggestions}
+      renderSuggestion={renderSuggestion} />
   )
 
 }
---
+++ src/ui/Autosuggest/Autosuggest.tsx
@@ -0,0 +1,67 @@
+import { Children, ReactNode, useState } from 'react';
+import ReactAutosuggest from 'react-autosuggest';
+import { cn } from '@/ui/utils';
+
+interface AutosuggestProps<T extends { id: string }> {
+
+  id?: string;
+
+  className?: string;
+
+  value: string;
+
+  getSuggestions: (query: string) => T[];
+
+  renderSuggestion: (suggestion: T) => ReactNode;
+
+  onChange(value: string): void;
+
+}
+
+export const Autosuggest = <T extends { id: string }>(props: AutosuggestProps<T>) => {
+ 
+  const [suggestions, setSuggestions] = useState([]);
+
+  const onGetSuggestions = ({ value }: { value: string }) =>
+    setSuggestions(props.getSuggestions(value));
+
+  const inputClass = cn(
+    'flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+    props.className
+  );
+
+  const containerClass = 
+    'react-autosuggest__suggestions-container absolute mt-1.5 w-full z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg'
+
+  const suggestionClass =
+    'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground';
+
+  const renderSuggestion = (suggestion: T, { isHighlighted }) => (
+    <div className={suggestionClass} data-highlighted={isHighlighted ? 'true' : undefined}>
+      {props.renderSuggestion(suggestion)}
+    </div>
+  )
+
+  return (
+    <ReactAutosuggest 
+      suggestions={suggestions} 
+      onSuggestionsFetchRequested={onGetSuggestions}
+      onSuggestionsClearRequested={() => setSuggestions([])}
+      getSuggestionValue={suggestion => suggestion.id}
+      renderSuggestionsContainer={({ containerProps, children }) => Children.toArray(children).length > 0 ? (
+        <div {...containerProps} className={containerClass}>
+          {children}
+        </div>
+      ) : null}
+      renderSuggestion={renderSuggestion}
+      containerProps={{
+        className: 'relative'
+      }}
+      inputProps={{
+        className: inputClass,
+        value: props.value,
+        onChange: (_, { newValue }) => props.onChange(newValue)
+      }} />
+  )
+  
+}
