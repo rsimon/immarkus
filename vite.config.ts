8fc0f467a7d9f288ae1f44dc2aca3de0ccbdb1f5
Rainer Simon <rainer@rainersimon.io>
2024-01-02 16:28:23 +0100

    Added vendor chunk plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
