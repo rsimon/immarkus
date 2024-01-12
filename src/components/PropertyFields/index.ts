7f0a67fb40583bb3426e96931edaf51f16a2856e
Rainer Simon <rainer@rainersimon.io>
2024-01-12 09:25:05 +0100

    Added 'Measurement' property type

--- src/components/PropertyFields/index.ts
+++ src/components/PropertyFields/index.ts
@@ -1,6 +1,7 @@
 export * from './EnumField';
 export * from './ExternalAuthorityField';
 export * from './GeoCoordinateField';
+export * from './MeasurementField';
 export * from './NumberField';
 export * from './TextField';
 export * from './URIField';
