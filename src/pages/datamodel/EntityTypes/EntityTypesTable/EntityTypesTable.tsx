e64906fd54bb15514006344d20d092b9892d3019
Rainer Simon <rainer@rainersimon.io>
2024-01-23 07:59:56 +0100

    First pass at tree table

--- src/pages/datamodel/EntityTypes/EntityTypesTable/EntityTypesTable.tsx
+++ src/pages/datamodel/EntityTypes/EntityTypesTable/EntityTypesTable.tsx
@@ -1,7 +1,9 @@
 import { CaseSensitive, Database, Hash, Link2, List, MapPin, Ruler } from 'lucide-react';
-import { EntityType } from '@/model';
-import { useDataModel } from '@/store';
+import { EntityType, PropertyDefinition } from '@/model';
+import { DataModel, useDataModel } from '@/store';
 import { EntityTypeActions } from '../EntityTypeActions';
+import { TreeTable } from 'primereact/treetable';
+import { Column } from 'primereact/column';
 import {
   Table,
   TableBody,
@@ -10,6 +12,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/ui/Table';
+import { TreeNode } from 'primereact/treenode';
 
 interface EntityTypesTableProps {
 
@@ -19,25 +22,102 @@ interface EntityTypesTableProps {
 
 }
 
+
+
 export const EntityTypesTable = (props: EntityTypesTableProps) => {
 
   const model = useDataModel();
 
-  return (
-    <div className="rounded-md border mt-6">
-      <Table>
-        <TableHeader className="text-xs">
-          <TableRow>
-            <TableHead colSpan={2} className="pl-3 pr-2 whitespace-nowrap">Entity Class</TableHead>
-            <TableHead className="px-2 whitespace-nowrap">Display Name</TableHead>
-            <TableHead className="px-2 whitespace-nowrap">Parent Class</TableHead>
-            <TableHead className="px-2 whitespace-nowrap w-[280px]">Description</TableHead>
-            <TableHead className="px-2 whitespace-nowrap w-[300px]">Properties</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
+  const toTreeNode = (type: EntityType) => ({
+    id: type.id,
+    key: type.id,
+    label: type.id,
+    data: { ...type },
+    get children() {
+      return model.getChildTypes(type.id).map(toTreeNode)
+    }
+  })
 
-        <TableBody>
+  const nodes: TreeNode[] = model.getRootTypes().map(toTreeNode)
+
+  const idTemplate = (node: TreeNode) => (
+    <>
+      <span className="pip" style={{ backgroundColor: node.data.color }} />
+      <span>{node.data.id}</span>
+    </>
+  )
+
+  const actionsTemplate = (node: TreeNode) => (
+    <span className="text-right py-1 px-2">
+      <EntityTypeActions 
+        onEditEntityType={() => props.onEditEntityType(node.data)} 
+        onDeleteEntityType={() => props.onDeleteEntityType(node.data)} />
+    </span>
+  )
+
+  const propertiesTemplate = (node: TreeNode) => (
+    <span>
+      {node.data.properties?.map((property: PropertyDefinition) => (
+        <span key={property.name}
+          className="align-middle inline-flex bg-muted-foreground/40 text-dark text-xs 
+            mx-0.5 mb-1 py-0.5 px-1.5 rounded-full items-center" style={{ fontSize: '0.65rem'}}>
+          {property.type === 'enum' ? (
+            <List className="w-3 h-3 mr-0.5" />
+          ): property.type === 'external_authority' ? (
+            <Database className="w-3 h-3 mr-1" />
+          ) : property.type === 'geocoordinate' ? (
+            <MapPin className="w-3 h-3 mr-0.5" />
+          ) : property.type === 'measurement' ? (
+            <Ruler className="w-3 h-3 mr-1" />
+          ) : property.type === 'number' ? (
+            <Hash className="w-3 h-3 mr-0.5" />
+          ) : property.type === 'text' ? (
+            <CaseSensitive className="w-4 h-4 mr-0.5" />
+          ) : property.type === 'uri' ? (
+            <Link2 className="w-3 h-3 mr-0.5" />
+          ) : null}
+          {property.name}
+        </span>    
+      ))}
+    </span>
+  )
+
+  const headerClass = "pl-3 pr-2 whitespace-nowrap text-xs text-muted-foreground font-semibold text-left";
+
+  return (
+    <div className="rounded-md border mt-6 w-full overflow-auto">
+      <TreeTable 
+        value={nodes} 
+        className="w-full caption-bottom text-sm">
+
+        <Column
+          expander
+          header="Entity Class" 
+          headerClassName={headerClass}
+          body={idTemplate} />
+
+        <Column 
+          header="Display Name" 
+          headerClassName={headerClass}
+          field="label" />
+
+        <Column 
+         header="Description"
+         headerClassName={headerClass}
+         field="description" />
+
+        <Column
+          header="Properties"
+          headerClassName={headerClass}
+          body={propertiesTemplate} />
+
+        <Column 
+          body={actionsTemplate} />
+      </TreeTable>
+    </div>
+  )
+
+  /*
           {model.entityTypes.length === 0 ? (
             <TableRow>
               <TableCell
@@ -79,17 +159,10 @@ export const EntityTypesTable = (props: EntityTypesTableProps) => {
                   </span>    
                 ))}
               </TableCell>
-              <TableCell className="text-right py-1 px-2">
-                <EntityTypeActions 
-                  onEditEntityType={() => props.onEditEntityType(e)} 
-                  onDeleteEntityType={() => props.onDeleteEntityType(e)} />
-              </TableCell>
+
             </TableRow>
           ))}
-        </TableBody>
-      </Table>
-    </div>
 
-  )
+  */
 
 }
