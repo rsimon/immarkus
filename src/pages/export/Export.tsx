e3b7a2b9720b0c6a09002722d6d20fd6ea88c900
Rainer Simon <rainer@rainersimon.io>
2024-01-24 13:48:40 +0100

    Proof of concept: bulk export for all annotations

--- src/pages/export/Export.tsx
+++ src/pages/export/Export.tsx
@@ -1,6 +1,15 @@
 import { AppNavigationSidebar } from '@/components/AppNavigationSidebar';
+import { useStore } from '@/store';
+import { Button } from '@/ui/Button';
+import { exportAnnotations } from './annotations/exportAnnotations';
 
 export const Export = () => {
+
+  const store = useStore();
+
+  const exportStuff = () => {
+    exportAnnotations(store);
+  }
 
   return (
     <div className="page-root">
@@ -12,6 +21,10 @@ export const Export = () => {
           This page is just a placeholder. In the future, we might (for example) offer 
           download options for exporting your data in different formats.
         </p>
+
+        <div className="mt-4">
+          <Button onClick={exportStuff}>Export</Button>
+        </div>
       </main>
     </div>
   )
---
+++ src/pages/export/annotations/exportAnnotations.ts
@@ -0,0 +1,40 @@
+import { Folder, Image, RootFolder } from '@/model';
+import { Store } from '@/store';
+import { W3CAnnotation } from '@annotorious/react';
+
+export const exportAnnotations = (store: Store) => {
+
+  const getImagesRecursive = (folder: RootFolder | Folder, allImages: Image[] = []): Image[] => {
+    const { images, folders } = store.getFolderContents(folder.handle);
+
+    const updatedImages = [...allImages, ...images];
+
+    return folders.reduce((all, subfolder) => getImagesRecursive(subfolder, all), updatedImages);
+  }
+
+  const images = getImagesRecursive(store.getRootFolder());
+
+  const annotations = images.reduce<Promise<W3CAnnotation[]>>((promise, image) => {
+    return promise.then((all) => {
+      return store.getAnnotations(image.id).then(annotations => {
+        return [...all, ...annotations]
+      })
+    })
+  }, Promise.resolve([]))
+
+  annotations.then(annotations => {
+    console.log('downloading', annotations);
+
+    const str = JSON.stringify(annotations);
+    const data = new TextEncoder().encode(str);
+    const blob = new Blob([data], {
+      type: 'application/json;charset=utf-8'
+    });
+
+    const anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(blob);
+    anchor.download = 'annotations.json';
+    anchor.click();
+  });
+
+}
