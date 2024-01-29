2aa46f4b95b438b5ddd9c3f7212ec5c7af9cfe2e
Rainer Simon <rainer@rainersimon.io>
2024-01-29 10:15:14 +0100

    Added folder/image metadata CRUD methods to store

--- src/store/datamodel/DataModelStore.ts
+++ src/store/datamodel/DataModelStore.ts
@@ -3,9 +3,12 @@ import { DataModel } from './DataModel';
 import { readJSONFile, writeJSONFile } from '../utils';
 import { EntityTypeTree, createEntityTypeTree } from './EntityTypeTree';
 import { removeMissingParentIds, repairDataModel } from '../integrity';
+import { ImageMetadataSchema } from '@/model/ImageMetadataSchema';
+import { FolderMetadataSchema } from '@/model/FolderMetadataSchema';
 
 export interface DataModelStore extends DataModel, EntityTypeTree {
 
+  // Entity types
   getEntityType(id: string, inheritProps?: boolean): EntityType | undefined;
 
   addEntityType(type: EntityType): Promise<void>;
@@ -13,6 +16,24 @@ export interface DataModelStore extends DataModel, EntityTypeTree {
   removeEntityType(typeOrId: EntityType | string): Promise<void>;
   
   updateEntityType(type: EntityType): Promise<void>;
+
+  // Folder metadata schemas
+  getFolderSchema(name: string): FolderMetadataSchema | undefined;
+
+  addFolderSchema(schema: FolderMetadataSchema): Promise<void>;
+
+  removeFolderSchema(schemaOrName: FolderMetadataSchema | string): Promise<void>;
+
+  updateFolderSchema(schema: FolderMetadataSchema): Promise<void>;
+
+  // Image metadata schemas
+  getImageSchema(name: string): ImageMetadataSchema | undefined;
+
+  addImageSchema(schema: ImageMetadataSchema): Promise<void>;
+
+  removeImageSchema(schemaOrName: ImageMetadataSchema | string): Promise<void>;
+
+  updateImageSchema(schema: ImageMetadataSchema): Promise<void>;
 
 }
 
@@ -24,9 +45,13 @@ export const loadDataModel = (
 
   const file = await fileHandle.getFile();
 
-  let { entityTypes } = (await readJSONFile<DataModel>(file) || {
+  let { entityTypes, imageSchemas, folderSchemas } = (await readJSONFile<DataModel>(file) || {
 
-    entityTypes: []
+    entityTypes: [],
+
+    folderSchemas: [],
+
+    imageSchemas: []
 
   });
 
@@ -34,19 +59,40 @@ export const loadDataModel = (
 
   const tree = createEntityTypeTree(entityTypes);
 
-  const rebuildAndSave = () => {
+  const rebuildEntityTypeTreeAndSave = () => {
     tree.rebuild(entityTypes);
-    return writeJSONFile(fileHandle, { entityTypes });
+    return writeJSONFile(fileHandle, { entityTypes, folderSchemas, imageSchemas });
   }
+
+  const save = () =>
+    writeJSONFile(fileHandle, { entityTypes, folderSchemas, imageSchemas });
 
   /** API **/
 
   const addEntityType = (type: EntityType) => {
     if (!entityTypes.find(e => e.id === type.id)) {
       entityTypes = [...entityTypes, type];
-      return rebuildAndSave();
+      return rebuildEntityTypeTreeAndSave();
     } else {
       return Promise.reject(`Entity with ID ${type.id} already exists`);
+    }
+  }
+
+  const addFolderSchema = (schema: ImageMetadataSchema) => {
+    if (!folderSchemas.find(s => s.name === schema.name)) {
+      folderSchemas = [...folderSchemas, schema];
+      return save();
+    } else {
+      return Promise.reject(`Folder schema "${schema.name}" already exists`);
+    }
+  }
+
+  const addImageSchema = (schema: ImageMetadataSchema) => {
+    if (!imageSchemas.find(s => s.name === schema.name)) {
+      imageSchemas = [...imageSchemas, schema];
+      return save();
+    } else {
+      return Promise.reject(`Image schema "${schema.name}" already exists`);
     }
   }
 
@@ -77,6 +123,12 @@ export const loadDataModel = (
     }
   }
 
+  const getFolderSchema = (name: string) =>
+    folderSchemas.find(s => s.name === name);
+
+  const getImageSchema = (name: string) =>
+    imageSchemas.find(s => s.name === name);
+
   const removeEntityType = (typeOrId: EntityType | string) => {
     const id = typeof typeOrId === 'string' ? typeOrId : typeOrId.id;
     const next = entityTypes.filter(e => e.id !== id);
@@ -84,25 +136,65 @@ export const loadDataModel = (
     // Repair the graph, in case the removed entity had children
     entityTypes = removeMissingParentIds(next);
 
-    return rebuildAndSave();
+    return rebuildEntityTypeTreeAndSave();
+  }
+
+  const removeFolderSchema = (schemaOrName: FolderMetadataSchema | string) => {
+    const name = typeof schemaOrName === 'string' ? schemaOrName : schemaOrName.name;
+    folderSchemas = folderSchemas.filter(s => s.name !== name);
+    return save();
+  }
+
+  const removeImageSchema = (schemaOrName: ImageMetadataSchema | string) => {
+    const name = typeof schemaOrName === 'string' ? schemaOrName : schemaOrName.name;
+    imageSchemas = imageSchemas.filter(s => s.name !== name);
+    return save();
   }
 
   const updateEntityType = (type: EntityType) => {
     if (entityTypes.find(e => e.id === type.id)) {
       entityTypes = entityTypes.map(e => e.id === type.id ? type : e);
-      return rebuildAndSave();
+      return rebuildEntityTypeTreeAndSave();
     } else {
       return Promise.reject(`Attempt to update entity ${type.id} but does not exist in store`);
+    }
+  }
+
+  const updateFolderSchema = (schema: FolderMetadataSchema) => {
+    if (folderSchemas.find(s => s.name === schema.name)) {
+      folderSchemas = folderSchemas.map(s => s.name === schema.name ? schema : s);
+      return save();
+    } else {
+      return Promise.reject(`Attempt to update folder scheam ${schema.name} but does not exist in store`);
+    }
+  }
+
+  const updateImageSchema = (schema: ImageMetadataSchema) => {
+    if (imageSchemas.find(s => s.name === schema.name)) {
+      imageSchemas = imageSchemas.map(s => s.name === schema.name ? schema : s);
+      return save();
+    } else {
+      return Promise.reject(`Attempt to update image scheam ${schema.name} but does not exist in store`);
     }
   }
 
   resolve({
     ...tree, 
     get entityTypes() { return entityTypes },
+    get folderSchemas() { return folderSchemas },
+    get imageSchemas() { return imageSchemas },
     addEntityType,
+    addFolderSchema,
+    addImageSchema,
     getEntityType,
+    getFolderSchema,
+    getImageSchema,
     removeEntityType,
-    updateEntityType
+    removeFolderSchema,
+    removeImageSchema,
+    updateEntityType,
+    updateFolderSchema,
+    updateImageSchema
   });
 
 });
