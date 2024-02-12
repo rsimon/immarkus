f9df3af5b82aad9a7ffe7eea56d72ff7043d4767
Rainer Simon <rainer@rainersimon.io>
2024-02-12 12:58:28 +0100

    Build tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import path from 'path';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'chart.js/auto',
+        'quill'
+      ],
+      output: {
+        manualChunks: {
+          'openseadragon': ['openseadragon'],
+          'primereact': ['primereact']
+        }
+      }
     }
   }
 })
