59ddc91b2e353f799126712fedfe2dd8c13c4c5e
Rainer Simon <rainer@rainersimon.io>
2024-02-21 16:00:35 +0100

    Added thumbnails to 'Add Image' list

--- src/pages/annotate/HeaderSection/AddImage/AddImage.tsx
+++ src/pages/annotate/HeaderSection/AddImage/AddImage.tsx
@@ -1,13 +1,13 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { ImagePlus } from 'lucide-react';
-import { Image } from '@/model';
+import { FolderItems, Image } from '@/model';
 import { useStore } from '@/store';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/ui/DropdownMenu';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/ui/Popover';
+import { Thumbnail } from '@/components/Thumbnail';
 
 interface AddImageProps {
 
@@ -21,29 +21,48 @@ export const AddImage = (props: AddImageProps) => {
 
   const store = useStore();
 
-  const { images } = store;
+  const [open, setOpen] = useState(false);
 
-  const currentIds = useMemo(() => new Set(props.current.map(image => image.id)), [props.current]);
+  const [filtered, setFiltered] = useState<FolderItems>(store.getFolderContents(store.getRootFolder().handle));
+
+  const addedImages = useMemo(() => new Set(props.current.map(image => image.id)), [props.current]);
+
+  const onAddImage = (image: Image) => {
+    setOpen(false);
+    props.onAddImage(image);
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
         className="p-2 flex items-center text-xs rounded-md hover:bg-muted focus-visible:outline-none 
           focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
         <ImagePlus className="h-4 w-4 mr-1" /> Add image
-      </DropdownMenuTrigger>
+      </PopoverTrigger>
 
-      <DropdownMenuContent className="max-h-[300px] overflow-y-auto">
-        {images.map(image => (
-          <DropdownMenuItem 
-            key={image.id}
-            disabled={currentIds.has(image.id)}
-            onSelect={() => props.onAddImage(image)}>
-            {image.name}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <PopoverContent className="w-[400px] p-0">
+        <div>
+          <input></input>
+        </div>
+        <div className="h-[420px] overflow-y-auto p-2">
+          <ul>
+            {filtered.images.map(image => (
+              <li key={image.id} className="mt-0.5 p-2 hover:bg-muted rounded-lg cursor-pointer">
+                <button 
+                  disabled={addedImages.has(image.id)}
+                  className={`flex gap-3 items-start text-xs ${addedImages.has(image.id) ? 'opacity-60' : ''}`}
+                  onClick={() => onAddImage(image)}>
+                  <Thumbnail image={image} /> 
+                  <div className="line-clamp-3 overflow-hidden text-ellipsis">
+                    {image.name}
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </PopoverContent>
+    </Popover>
   )
 
 }
