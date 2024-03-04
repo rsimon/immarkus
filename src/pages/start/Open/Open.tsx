d2591e282621b1716673f28c25d739d1d76c2f51
Rainer Simon <rainer@rainersimon.io>
2024-03-04 15:01:34 +0100

    Formatting

--- src/pages/start/Open/Open.tsx
+++ src/pages/start/Open/Open.tsx
@@ -47,10 +47,10 @@ export const Open = (props: OpenProps) => {
 
       <footer className="text-xs p-4 w-full flex justify-center">
         <div className="max-w-4xl text-center text-muted-foreground leading-relaxed">
-        Developed by Prof. Dr. Hilde De Weerdt, Dr. Rainer Simon, Dr. Lee Sunkyu, Dr. 
-        Iva Stojević, Meret Meister, and Xi Wangzhi with funding from the European 
-        Research Council under the Horizon 2020 programme, grant agreement No. 
-        101019509. <a className="text-sky-700 underline" href="https://github.com/rsimon/immarkus/wiki" target="_blank">Learn more</a>.
+          Developed by Prof. Dr. Hilde De Weerdt, Dr. Rainer Simon, Dr. Lee Sunkyu, Dr. 
+          Iva Stojević, Meret Meister, and Xi Wangzhi with funding from the European 
+          Research Council under the Horizon 2020 programme, grant agreement No. 
+          101019509. <a className="text-sky-700 underline" href="https://github.com/rsimon/immarkus/wiki" target="_blank">Learn more</a>.
         </div>
       </footer>
     </div>
