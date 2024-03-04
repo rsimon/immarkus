a10606fe42b93fee1574a0ad9622c12ecb7ca33d
Rainer Simon <rainer@rainersimon.io>
2024-03-04 15:49:58 +0100

    Progress dialog, style tweaks

--- src/pages/export/ExportAnnotations/ExportAnnotations.tsx
+++ src/pages/export/ExportAnnotations/ExportAnnotations.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { Binary, Download } from 'lucide-react';
+import { FileBarChart2, FileJson, Table2 } from 'lucide-react';
 import { Spinner } from '@/components/Spinner';
 import { useStore } from '@/store';
 import { Button } from '@/ui/Button';
-import { Dialog, DialogContent, DialogTrigger } from '@/ui/Dialog';
+import { Dialog, DialogTitle, DialogContent } from '@/ui/Dialog';
 import { Progress } from '@/ui/Progress';
 import { exportAnnotationsAsExcel } from './exportExcel';
 import { exportAnnotationsAsJSONLD } from './exportJSONLD';
@@ -34,62 +34,69 @@ export const ExportAnnotations = () => {
     <> 
       <ul className="py-2">
         <li>
-          <div className="w-full py-2 flex flex-row gap-20 justify-between">
-            <div>
-              <h3 className="font-medium mb-1 leading-relaxed">
-                All Annotations
-              </h3>
+          <div className="max-w-xl py-2">
+            <h3 className="font-medium leading-relaxed">
+              Annotation Data
+            </h3>
 
-              <p className="text-sm">
-                All annotations, on all images in your current work folder, as a flat list
-                in <a className="underline underline-offset-4 hover:text-primary" href="https://www.w3.org/TR/annotation-model/" target="_blank">W3C Web Annotation format</a>.
-              </p>
-            </div>
+            <p className="text-sm pt-3 pb-5 leading-relaxed">
+              All annotations, on all images in your current work folder, as a flat list
+              in <a 
+                className="underline underline-offset-4 hover:text-primary" 
+                href="https://www.w3.org/TR/annotation-model/" target="_blank">W3C Web Annotation</a> JSON-LD format.
+            </p>
 
-            <div>
-              <Button 
-                className="whitespace-nowrap flex gap-2 w-32"
-                onClick={() => exportAnnotationsAsJSONLD(store)}>
-                <Download className="h-4 w-4" /> JSON-LD
-              </Button>
-            </div>
+            <Button
+              className="whitespace-nowrap flex gap-3 w-36"
+              onClick={() => exportAnnotationsAsJSONLD(store)}>
+              <FileJson className="h-5 w-5" /> JSON-LD
+            </Button>
           </div>
         </li>
 
         <li>
-          <div className="w-full py-2 flex flex-row gap-20 justify-between">
-            <div>
-              <h3 className="font-medium mb-1 leading-relaxed">
-                Annotations and Images
-              </h3>
+          <div className="max-w-xl pt-16">
+            <h3 className="font-medium leading-relaxed">
+              Annotations and Images
+            </h3>
 
-              <p className="text-sm">
-                All annotations, on all images in your current work folder, as an Excel file. This export 
-                will also include the annotated images snippets.
-              </p>
-            </div>
+            <p className="text-sm pt-3 pb-5 leading-relaxed">
+              An export of all annotations, on all images in your current work folder, as an Excel file. 
+              Each top-level Entity Class will be on a separate worksheet. Image snippets are included
+              as a spreadhseet column.
+            </p>
 
-            <div>
-              <Button 
-                disabled={busy}
-                className="whitespace-nowrap flex gap-2 w-32"
-                onClick={onExportXLSX}>
-                {busy ? (
-                  <Spinner className="w-4 h-4 text-white" />
-                ) : (
-                  <><Download className="h-4 w-4" /> XLS</>
-                )}
-              </Button>
-            </div>
+            <Button 
+              disabled={busy}
+              className="whitespace-nowrap flex gap-3 w-36"
+              onClick={onExportXLSX}>
+              {busy ? (
+                <Spinner className="w-4 h-4 text-white" />
+              ) : (
+                <><FileBarChart2 className="h-5 w-5" /> XLSX</>
+              )}
+            </Button>
           </div>
         </li>
       </ul>
 
-      <Dialog open={!busy}>
+      <Dialog open={busy}>
         <DialogContent>
-          Processing
+          <DialogTitle className="flex items-center gap-2">
+            <Table2 className="h-5 w-5" /> Processing
+          </DialogTitle>
 
-          <Progress value={progress} />
+          <div className="pb-4">
+            <p className="text-sm leading-relaxed pt-6 pb-3 px-0.5">
+              Exporting XLSX. This may take a while.
+            </p>
+
+            <Progress value={progress} />
+
+            <div className="text-center text-muted-foreground text-sm pt-3">
+              {Math.round(progress)} %
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
     </>
