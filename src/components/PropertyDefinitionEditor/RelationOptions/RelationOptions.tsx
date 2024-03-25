44a1fdc0bace041c52d925c6f88415a1457ddb78
Rainer Simon <rainer@rainersimon.io>
2024-03-25 13:34:09 +0100

    Replaced entity type browser with autocomplete search

--- src/components/PropertyDefinitionEditor/RelationOptions/RelationOptions.tsx
+++ src/components/PropertyDefinitionEditor/RelationOptions/RelationOptions.tsx
@@ -1,4 +1,5 @@
 import { EntityTypeBrowser } from '@/components/EntityTypeBrowser';
+import { EntityTypeSearchSimple } from '@/components/EntityTypeSearchSimple';
 import { EntityType, RelationPropertyDefinition } from '@/model';
 import { useDataModel } from '@/store';
 import { Label } from '@/ui/Label';
@@ -25,28 +26,25 @@ export const RelationOptions = (props: RelationOptionsProps) => {
   const target = props.definition.targetType ?
     datamodel.getEntityType(props.definition.targetType, true) : undefined;
 
-  const onChangeTargetType = (type: EntityType) =>
-    props.onUpdate({...props.definition, targetType: type.id });
+  const onChangeTargetType = (type: string) =>
+    props.onUpdate({...props.definition, targetType: type });
 
   const onChangeLabelProperty = (name: string) =>
     props.onUpdate({ ...props.definition, labelProperty: name });
 
   return (
-    <div className="bg-muted px-2 py-3 mt-2 rounded-md text-sm">
-      <p>
-        
-      </p>
-
-      <div className="mt-4">
+    <div className="bg-muted px-3 pt-3 pb-4 mt-4 mb-6 rounded-md text-sm">
+      <div>
         <Label 
           htmlFor="name"
           className="inline-block text-xs mb-1.5 ml-0.5">
           Target Entity Class
         </Label>
 
-        <EntityTypeBrowser 
-          onSelect={onChangeTargetType}
-          onConfirm={onChangeTargetType} />
+        <EntityTypeSearchSimple 
+          className="bg-white"
+          value={props.definition.targetType} 
+          onChange={onChangeTargetType} />
       </div>
 
       <div className="mt-4">
