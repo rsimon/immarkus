70216fa37de995b8d28ae85d8991ecae06f324bc
Rainer Simon <rainer@rainersimon.io>
2024-03-26 10:04:51 +0100

    Input validation for target class search

--- src/components/PropertyDefinitionEditor/RelationOptions/RelationOptions.tsx
+++ src/components/PropertyDefinitionEditor/RelationOptions/RelationOptions.tsx
@@ -1,6 +1,7 @@
-import { EntityTypeBrowser } from '@/components/EntityTypeBrowser';
+import { useEffect, useState } from 'react';
+import { AlertCircle } from 'lucide-react';
 import { EntityTypeSearchSimple } from '@/components/EntityTypeSearchSimple';
-import { EntityType, RelationPropertyDefinition } from '@/model';
+import { RelationPropertyDefinition } from '@/model';
 import { useDataModel } from '@/store';
 import { Label } from '@/ui/Label';
 import {
@@ -23,14 +24,26 @@ export const RelationOptions = (props: RelationOptionsProps) => {
 
   const datamodel = useDataModel();
 
-  const target = props.definition.targetType ?
-    datamodel.getEntityType(props.definition.targetType, true) : undefined;
+  const [value, setValue] = useState(props.definition.targetType);
+  
+  const target = value ? datamodel.getEntityType(value, true) : undefined;
 
-  const onChangeTargetType = (type: string) =>
-    props.onUpdate({...props.definition, targetType: type, labelProperty: undefined });
+  const onChangeTargetType = (type: string) => {
+    setValue(type);
+
+    const isValid = Boolean(datamodel.getEntityType(type));
+    if (isValid)
+      props.onUpdate({...props.definition, targetType: type, labelProperty: undefined });
+  }
 
   const onChangeLabelProperty = (name: string) =>
     props.onUpdate({ ...props.definition, labelProperty: name });
+
+  useEffect(() => {
+    setValue(props.definition.targetType);
+  }, [props.definition])
+
+  const invalidTargetClass = value && !target;
 
   return (
     <div className="bg-muted px-3 pt-3 pb-4 mt-4 mb-6 rounded-md text-sm">
@@ -51,8 +64,15 @@ export const RelationOptions = (props: RelationOptionsProps) => {
 
         <EntityTypeSearchSimple 
           className="bg-white"
-          value={props.definition.targetType} 
+          value={value} 
           onChange={onChangeTargetType} />
+
+        {invalidTargetClass && (
+          <span className="flex items-center text-xs px-0.5 mt-2 text-red-600 whitespace-nowrap">
+            <AlertCircle className="flex-shrink-0 h-3.5 w-3.5 mb-0.5 ml-0.5 mr-1" /> 
+            <>Must be a valid entity class ID</>
+          </span>
+        )}
       </div>
 
       <div className="mt-4">
@@ -71,7 +91,11 @@ export const RelationOptions = (props: RelationOptionsProps) => {
           </SelectTrigger>
 
           <SelectContent>
-            {target && (target.properties || []).map(property => (
+            {(target?.properties || []).length === 0 ? (
+              <span className="text-muted-foreground text-sm p-2 flex justify-center">
+                No properties
+              </span>
+            ) : target.properties.map(property => (
               <SelectItem 
                 key={property.name}
                 value={property.name}>
