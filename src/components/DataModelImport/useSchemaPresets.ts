be0b8fbb7082e545f17ca6b5c4188c2b9e7ac61d
Rainer Simon <rainer@rainersimon.io>
2024-06-24 13:09:42 +0200

    Metadata template import

---
+++ src/components/DataModelImport/useSchemaPresets.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { ModelPreset, useRuntimeConfig } from '@/RuntimeConfig';
+import { MetadataSchema } from '@/model';
+
+export const useSchemaPresets = (type: 'ENTITY_TYPES' | 'FOLDER_SCHEMAS' | 'IMAGE_SCHEMAS') => {
+
+  const config = useRuntimeConfig();
+
+  const [schemas, setSchemas] = useState<MetadataSchema[]>([]);
+
+  useEffect(() => {
+    const presets = (config.model_presets || []).reduce<ModelPreset[]>((all, next) => {
+      if (next.type === type) {
+        return [...all, next];
+      } else {
+        return all;
+      }
+    }, []);
+
+    const promise = presets.reduce<Promise<MetadataSchema[]>>((promise, preset) => promise.then(all => {
+      return fetch(preset.url)
+        .then(res => res.json())
+        .catch(() => {
+          return all;
+        })
+        .then(schemas => {
+          return [...all, ...schemas];
+        })
+    }), Promise.resolve<MetadataSchema[]>([]));
+    
+    promise.then(setSchemas);
+  }, [type]);
+
+  return schemas;
+
+}
