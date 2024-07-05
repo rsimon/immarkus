2053fc59f7970c0fea4ee17b9de88ae57b0e683a
Rainer Simon <rainer@rainersimon.io>
2024-07-05 16:58:06 +0200

    Cleanup

--- src/store/hooks/useImageSnippet.ts
+++ src/store/hooks/useImageSnippet.ts
@@ -42,37 +42,3 @@ export const useImageSnippet = (annotation: W3CImageAnnotation) => {
   return snippets && snippets.length > 0 ? snippets[0] : undefined;
 
 }
-
-/*
-export const useImageSnippet = (annotation: W3CImageAnnotation) => {
-
-  const store = useStore();
-
-  const [loadedImage, setLoadedImage] = useState<LoadedImage | undefined>();
-
-  const [snippet, setSnippet] = useState<ImageSnippet | undefined>();
-
-  const image = useMemo(() => (
-    store.images.find(i => i.name === (annotation.target as any).source)
-  ), [annotation]);
-
-  useEffect(() => {
-    if (!image) return;
-
-    store.loadImage(image.id).then(setLoadedImage);
-  }, [annotation, image]);
-
-  useEffect(() => {
-    if (!loadedImage) return;
-
-    if ('selector' in annotation.target) {
-      getImageSnippet(loadedImage, annotation as W3CImageAnnotation)
-        .then(setSnippet);
-    }
-  }, [annotation, loadedImage]);
-
-  return snippet;
-
-}
-*/
-
