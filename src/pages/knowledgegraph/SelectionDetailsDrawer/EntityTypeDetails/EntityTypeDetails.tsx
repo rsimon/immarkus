89feedba81650ceef6f4f425bcbf26e5eebeaad9
Rainer Simon <rainer@rainersimon.io>
2024-07-15 13:03:25 +0200

    Entity type details sidebar changes according to graph mode

--- src/pages/knowledgegraph/SelectionDetailsDrawer/EntityTypeDetails/EntityTypeDetails.tsx
+++ src/pages/knowledgegraph/SelectionDetailsDrawer/EntityTypeDetails/EntityTypeDetails.tsx
@@ -4,7 +4,7 @@ import { EntityBadge } from '@/components/EntityBadge';
 import { EntityType } from '@/model';
 import { RelationGraph } from '@/store';
 import { Button } from '@/ui/Button';
-import { Graph } from '../../Types';
+import { Graph, KnowledgeGraphSettings } from '../../Types';
 import { EntityAnnotations } from './EntityAnnotations';
 import { Spline } from 'lucide-react';
 import { RelatedAnnotationList } from './RelatedAnnotationList';
@@ -14,6 +14,8 @@ interface EntityTypeDetailsProps {
   graph: Graph;
 
   relations: RelationGraph;
+
+  settings: KnowledgeGraphSettings;
 
   type: EntityType
 
@@ -47,41 +49,45 @@ export const EntityTypeDetails = (props: EntityTypeDetailsProps) => {
         )}
       </div>
 
-      <div className="pb-1.5">
-        {linkedNodes.map(node => (
-          <section 
-            key={node.id} 
-            className="p-4 border-t">
-            <h3 className="text-xs mb-3 font-medium whitespace-nowrap overflow-hidden text-ellipsis">
-              {node.label}
-            </h3>
+      {props.settings.graphMode === 'HIERARCHY' && (
+        <div className="pb-1.5">
+          {linkedNodes.map(node => (
+            <section 
+              key={node.id} 
+              className="p-4 border-t">
+              <h3 className="text-xs mb-3 font-medium whitespace-nowrap overflow-hidden text-ellipsis">
+                {node.label}
+              </h3>
 
-            <EntityAnnotations 
-              key={node.id}
-              imageId={node.id} 
-              entityType={type} />
+              <EntityAnnotations 
+                key={node.id}
+                imageId={node.id} 
+                entityType={type} />
 
-            <p>
-              <Button
-                size="sm"
-                variant="secondary"
-                className="mt-3 text-xs"
-                onClick={() => onOpen(node.id)}>
-                Open Image
-              </Button>
-            </p>
-          </section>
-        ))}
-      </div>
+              <p>
+                <Button
+                  size="sm"
+                  variant="secondary"
+                  className="mt-3 text-xs"
+                  onClick={() => onOpen(node.id)}>
+                  Open Image
+                </Button>
+              </p>
+            </section>
+          ))}
+        </div>
+      )}
 
-      <div className="border-t">
-        <h2 className="p-4 flex items-center text-sm font-semibold">
-          <Spline className="w-4 h-4 mr-1.5" /> Related
-        </h2>
+      {props.settings.graphMode === 'RELATIONS' && (
+        <div className="border-t">
+          <h2 className="p-4 flex items-center text-sm font-semibold">
+            <Spline className="w-4 h-4 mr-1.5" /> Related
+          </h2>
 
-        <RelatedAnnotationList 
-          related={relatedAnnotations} />
-      </div>
+          <RelatedAnnotationList 
+            related={relatedAnnotations} />
+        </div>
+      )}
     </aside>
   )
   
