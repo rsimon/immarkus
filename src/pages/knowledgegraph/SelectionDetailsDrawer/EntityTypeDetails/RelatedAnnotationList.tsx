67d950ff7cad7fe9626bfb0685a4998cd28f4618
Rainer Simon <rainer@rainersimon.io>
2024-07-26 10:25:17 +0200

    KG/Relations mode: when selecting an entity, sidebar now displays links to images

--- src/pages/knowledgegraph/SelectionDetailsDrawer/EntityTypeDetails/RelatedAnnotationList.tsx
+++ src/pages/knowledgegraph/SelectionDetailsDrawer/EntityTypeDetails/RelatedAnnotationList.tsx
@@ -1,4 +1,6 @@
 import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
+import { Image } from 'lucide-react';
 import { W3CImageAnnotation } from '@annotorious/react';
 import { RelatedAnnotation, useImageSnippets, useStore } from '@/store';
 import { ImageSnippet } from '@/utils/getImageSnippet';
@@ -67,14 +69,26 @@ export const RelatedAnnotationList = (props: RelatedAnnotationList) => {
           <li 
             key={r.annotation.id}
             className="flex items-end gap-2 text-sm mb-2">
+              
             <Thumbnail related={r} snippets={snippets} />
             
-            <div className="pb-1 flex flex-col gap-0.5">
+            <div className="pb-1 flex flex-col gap-0.5 text-xs overflow-hidden">
               <div>
                 <EntityBadge entityType={model.getEntityType(r.sourceEntityType)} />
               </div>
+
               <div>
                 {r.relationName}:{r.targetInstance}
+              </div>
+
+              <div className="flex items-center gap-1 text-muted-foreground">
+                <Image className="w-3.5 h-3.5" />
+
+                <Link 
+                  to={`../annotate/${r.image.id}`}
+                  className="whitespace-nowrap block max-w-full overflow-hidden text-ellipsis italic text-sky-700 hover:underline">
+                  {r.image.name}
+                </Link>
               </div>
             </div>
           </li>
