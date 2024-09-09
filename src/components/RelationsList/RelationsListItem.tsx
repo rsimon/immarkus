358cd67b970ef2765f3724aee8cdc40e227339ce
Rainer Simon <rainer@rainersimon.io>
2024-09-09 07:09:50 +0200

    Minor tweaks

--- src/components/RelationsList/RelationsListItem.tsx
+++ src/components/RelationsList/RelationsListItem.tsx
@@ -27,11 +27,11 @@ export const RelationsListItem = (props: RelationsListItemProps) => {
           className={fromId === refId ? 'shadow h-12 w-12' : 'shadow h-9 w-9'} /> 
       </div>
 
-      <div className="flex-grow h-[1px] border-gray-700 border-t border-dashed relative">
+      <div className="flex-grow h-[1px] border-gray-500 border-t-2 border-dashed relative">
         <div className="absolute -top-[0.75rem] w-full text-center">
           <span className="bg-white px-1">{props.relationship || 'to'}</span>
         </div>
-        <div className="absolute -right-0.5 -top-[5.5px] border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[12px] border-l-gray-700"></div>
+        <div className="absolute -right-0.5 -top-[6px] border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[12px] border-l-gray-500"></div>
       </div>
 
       <div className={toId === refId ? undefined : 'p-1.5'}>
