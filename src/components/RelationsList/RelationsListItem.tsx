5098d934764b6ff88c77fd96964a59b0dd935a07
Rainer Simon <rainer@rainersimon.io>
2024-10-02 11:16:02 +0200

    CSS tweaks, consistency

--- src/components/RelationsList/RelationsListItem.tsx
+++ src/components/RelationsList/RelationsListItem.tsx
@@ -36,20 +36,20 @@ export const RelationsListItem = (props: RelationsListItemProps) => {
         <div className={fromId === refId ? undefined : 'p-1.5'}>
           <AnnotationThumbnail 
             annotation={fromId} 
-            className={fromId === refId ? 'shadow h-12 w-12' : 'shadow h-9 w-9'} /> 
+            className={fromId === refId ? 'border shadow-sm h-12 w-12' : 'border shadow-sm h-9 w-9'} /> 
         </div>
 
-        <div className="flex-grow h-[1px] border-gray-500 border-t-2 border-dashed relative">
+        <div className="flex-grow h-[1px] border-gray-400 border-t border-dashed relative">
           <div className="absolute -top-[0.75rem] w-full text-center">
-            <span className="bg-white px-1">{relationship || 'to'}</span>
+            <span className="bg-white px-1 font-light text-muted-foreground">{relationship || 'to'}</span>
           </div>
-          <div className="absolute -right-0.5 -top-[6px] border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[12px] border-l-gray-500"></div>
+          <div className="absolute -right-0.5 -top-[5.5px] border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[12px] border-l-gray-400"></div>
         </div>
 
         <div className={toId === refId ? undefined : 'p-1.5'}>
           <AnnotationThumbnail 
             annotation={props.toId} 
-            className={toId === refId ? 'shadow h-12 w-12' : 'shadow h-9 w-9'} />
+            className={toId === refId ? 'border shadow-sm h-12 w-12' : 'border shadow-sm h-9 w-9'} />
         </div>
       </div>
 
