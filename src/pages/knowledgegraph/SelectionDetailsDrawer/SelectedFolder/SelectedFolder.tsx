2aaf547b5340054db9a4b4f6ca8465c50f7425d1
Rainer Simon <rainer@rainersimon.io>
2024-10-18 14:41:36 +0200

    KG: selected folder panel enhancements

--- src/pages/knowledgegraph/SelectionDetailsDrawer/SelectedFolder/SelectedFolder.tsx
+++ src/pages/knowledgegraph/SelectionDetailsDrawer/SelectedFolder/SelectedFolder.tsx
@@ -1,11 +1,12 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FolderIcon, FolderOpen, ImageIcon, X } from 'lucide-react';
 import { W3CAnnotationBody } from '@annotorious/react';
 import { PropertyValidation } from '@/components/PropertyFields';
 import { FolderMetadataForm, hasChanges } from '@/components/MetadataForm';
 import { Folder } from '@/model';
-import { useFolderMetadata } from '@/store';
+import { useFolderMetadata, useStore } from '@/store';
 import { Button } from '@/ui/Button';
-import { X } from 'lucide-react';
 
 interface SelectedFolderProps {
 
@@ -17,7 +18,16 @@ interface SelectedFolderProps {
 
 export const SelectedFolder = (props: SelectedFolderProps) => {
 
-  const { metadata, updateMetadata } = useFolderMetadata(props.folder);
+  const { folder } = props;
+
+  const store = useStore();
+
+  const [imageCount, folderCount] = useMemo(() => {
+    const items = store.getFolderContents(folder.handle);
+    return [items.images.length, items.folders.length];
+  }, [folder, store]);
+
+  const { metadata, updateMetadata } = useFolderMetadata(folder);
 
   const [formState, setFormState] = useState<W3CAnnotationBody | undefined>();
 
@@ -31,37 +41,68 @@ export const SelectedFolder = (props: SelectedFolderProps) => {
   }
 
   return (
-    <PropertyValidation>
-      <div className="p-2">
-        <form 
-          className="bg-white relative rounded border shadow-sm min-h-56 flex flex-col p-3 pt-12"
-          onSubmit={onSubmit}>
+    <div className="p-2">
+      <article className="flex flex-col">
+        <div className="bg-white rounded border shadow-sm">
+          <div className="px-3 py-2 flex justify-between items-center">
+            <h2 className="flex gap-1.5 items-center text-sm">
+              <FolderOpen className="h-4 w-4" /> {folder.name}
+            </h2>
 
-          <Button 
-            className="absolute h-8 w-8 right-1.5 top-1.5 rounded-full"
-            size="icon"
-            variant="ghost"
-            onClick={props.onClose}>
-            <X className="h-4 w-4" />
-          </Button>
-
-          <div className="flex flex-col flex-grow">          
-            <FolderMetadataForm
-              metadata={formState}
-              onChange={setFormState} />
-          </div>
-
-          <div className="pt-2">        
             <Button 
-              disabled={!hasChanges(metadata, formState)} 
-              className="w-full mb-2"
-              type="submit">
-              Save
+              className="h-8 w-8 right-1.5 top-1.5 rounded-full"
+              size="icon"
+              variant="ghost"
+              onClick={props.onClose}>
+              <X className="h-4 w-4" />
             </Button>
           </div>
-        </form>
-      </div>
-    </PropertyValidation> 
+
+          <div className="px-3 py-2 flex justify-between border-t items-center text-xs">
+            <div className="text-muted-foreground flex gap-4">
+              {imageCount > 0 && (
+                <div className="flex gap-1 items-center">
+                  <ImageIcon className="h-3.5 w-3.5" /> {imageCount} Images
+                </div>
+              )}
+
+              {folderCount > 0 && (
+                <div className="flex gap-1 items-center">
+                  <FolderIcon className="h-3.5 w-3.5" /> {folderCount} Sub-Folders
+                </div>
+              )}
+            </div>
+
+            <Link 
+              to={`/images/${folder.id}`}
+              className="bg-primary hover:bg-primary/90 text-white rounded-full px-4 py-1.5">
+              Open
+            </Link>
+          </div>
+        </div>
+
+        <PropertyValidation>
+          <form 
+            onSubmit={onSubmit} 
+            className="bg-white p-3 rounded border shadow-sm mt-2">
+            <div className="flex flex-col flex-grow">          
+              <FolderMetadataForm
+                metadata={formState}
+                onChange={setFormState} />
+            </div>
+
+            <div className="pt-2">        
+              <Button 
+                disabled={!hasChanges(metadata, formState)} 
+                className="w-full mb-2"
+                type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </PropertyValidation> 
+      </article>
+    </div>
   )
 
 }
