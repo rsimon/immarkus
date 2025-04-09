4c1e7630a901ad20d9212184c2e73cb34da78486
Rainer Simon <rainer@rainersimon.io>
2025-04-09 11:13:58 +0200

    First working version

--- src/pages/images/IIIFManifestGrid/IIIFManifestGrid.tsx
+++ src/pages/images/IIIFManifestGrid/IIIFManifestGrid.tsx
@@ -1,13 +1,14 @@
 import { useMemo } from 'react';
 import murmur from 'murmurhash';
 import { useNavigate, useParams } from 'react-router-dom';
+import { CozyCanvas, CozyRange } from 'cozy-iiif';
 import { CanvasInformation, IIIFManifestResource } from '@/model';
 import { Skeleton } from '@/ui/Skeleton';
 import { useIIIFResource } from '@/utils/iiif/hooks';
-import { CozyCanvas } from 'cozy-iiif';
 import { useManifestAnnotations } from '@/store/hooks';
 import { IIIFCanvasItem } from './IIIFCanvasItem';
 import { CanvasGridItem, GridItem } from '../Types';
+import { IIIFRangeItem } from './IIIFRangeItem';
 
 interface IIIFManifestGridProps {
 
@@ -27,7 +28,7 @@ export const IIIFManifestGrid = (props: IIIFManifestGridProps) => {
 
   const { folder: folderId } = useParams();
 
-  const [_, rangeId] = useMemo(() => folderId.split('@'), [folderId]);
+  const [canvasId, rangeId] = useMemo(() => folderId.split('@'), [folderId]);
   
   const navigate = useNavigate();
 
@@ -55,6 +56,55 @@ export const IIIFManifestGrid = (props: IIIFManifestGridProps) => {
     navigate(`/annotate/iiif:${props.manifest.id}:${id}`);
   }
 
+  const onOpenRange = (range: CozyRange) => {
+    const id = murmur.v3(range.id);
+    navigate(`/images/${props.manifest.id}@${id}`);
+  }
+
+  const { folders, canvases } = useMemo(() => {
+    if (!parsedManifest) return { folders: [], canvases: [] };
+
+    const toc = parsedManifest.getTableOfContents();
+
+    // ToC viewing mode unless all ToC nodes are leaf nodes
+    const renderAsToC = !toc.root.every(node => node.navSections.length === 0);
+    if (renderAsToC) {
+      const thisRange = rangeId ? parsedManifest.structure.find(range => {
+        const hash = murmur.v3(range.id);
+        return rangeId === hash.toString();
+      }) : undefined;
+
+      if (thisRange) {
+        const thisNode = toc.getNode(thisRange.id);
+        const { navItems, navSections: folders } = thisNode;
+
+        const canvases = navItems.map(i => props.manifest.canvases.find(c => c.uri === i.id));
+        return { folders, canvases };
+      } else if (!rangeId) {
+        // Root 
+        const { root } = toc;
+
+        const folders = root.filter(n => (n.navItems.length + n.navSections.length) > 0).map(n => n.source) as CozyRange[];
+        const navItems = root.filter(n => (n.navItems.length + n.navSections.length) === 0);
+
+        const canvases = navItems.map(i => props.manifest.canvases.find(c => c.uri === i.id));
+        return { folders, canvases };
+      } else {
+        // Invalid range ID!
+        return { folders: [], canvases: [] };
+      }
+    } else {
+      // No ToC - render all canvases flat
+      return { folders: [], canvases: props.manifest.canvases };
+    }
+  }, [rangeId, props.manifest, parsedManifest])
+
+  const filtered: CanvasInformation[] = useMemo(() => {
+    if (!props.hideUnannotated) return canvases;
+
+    return canvases.filter(c => annotationsByCanvas[c.id] > 0);
+  }, [canvases, props.hideUnannotated, annotationsByCanvas]);
+
   const renderCanvasItem = (info: CanvasInformation, canvas: CozyCanvas) => {
     const item: CanvasGridItem = ({ type: 'canvas', canvas, info });
 
@@ -71,73 +121,32 @@ export const IIIFManifestGrid = (props: IIIFManifestGridProps) => {
         annotationCount={annotationsByCanvas[id]}
         onOpen={() => onOpenCanvas(canvas)} 
         onSelect={() => props.onSelect(item)} />
-      );
+    );
   }
-
-  // For testing: 3370436377 ("Binding")
-  const gridItems = useMemo(() => {
-    if (!parsedManifest) return [];
-
-    const toc = parsedManifest.getTableOfContents();
-
-    // ToC viewing mode unless all ToC nodes are leaf nodes
-    const renderAsToC = !toc.every(node => {
-      // A true leaf node, with no children
-      const isLeafNode = node.children.length === 0;
-
-      // A defacto leaf, with only a single child that's a canvas.
-      const isLeafRange = node.children.length === 1 && node.children[0].type === 'canvas';
-
-      return isLeafNode || isLeafRange;
-    });
-
-    console.log('render as ToC', renderAsToC);
-
-    if (renderAsToC) {
-      // Current 'folder root' range, if any
-      const currentRange = rangeId ? parsedManifest.structure.find(range => {
-        const hash = murmur.v3(range.id);
-        return rangeId === hash.toString();
-      }) : undefined;
-
-      console.log('current range:', currentRange.getLabel());
-
-      if (currentRange) {
-        // We'll flatten child ranges that have 
-        // - only a single canvas child 
-        // - and no nested child ranges
-        const flattenedCanvases = currentRange.ranges.reduce<CozyCanvas[]>((agg, range) => {
-          const shouldFlatten = range.canvases.length === 1 && range.ranges.length === 0;
-          return shouldFlatten ? [...agg, ...range.canvases] : agg;
-        }, currentRange.canvases);
-
-        const canvasesInThisRange = new Set(flattenedCanvases.map(c => c.id));
-        return props.manifest.canvases.filter(info => canvasesInThisRange.has(info.uri));
-      } else {
-        return [];
-      }
-    } else {
-      // No ToC - render all canvases flat
-      return props.manifest.canvases;
-    }
-  }, [rangeId, props.manifest, parsedManifest])
-
-  const filtered: CanvasInformation[] = useMemo(() => {
-    if (!props.hideUnannotated) return gridItems;
-
-    return gridItems.filter(c => annotationsByCanvas[c.id] > 0);
-  }, [gridItems, props.hideUnannotated, annotationsByCanvas]);
 
   return (
     <div className="item-grid">
       {parsedManifest ? (
-        <ul>
-          {filtered.map((canvas, idx) => (
-            <li key={`${canvas.id}.${idx}`}>
-              {renderCanvasItem(canvas, parsedManifest.canvases.find(c => c.id === canvas.uri))}
-            </li>
-          ))}
-        </ul>
+        <>
+          {folders.length > 0 && (
+            <ul>
+              {folders.map((folder, idx) => (
+                <li key={`${folder.id}:${idx}`}>
+                  <IIIFRangeItem 
+                    range={folder} 
+                    onOpen={() => onOpenRange(folder)} />
+                </li>
+              ))}
+            </ul>
+          )}
+          <ul>
+            {filtered.map((canvas, idx) => (
+              <li key={`${canvas.id}:${idx}`}>
+                {renderCanvasItem(canvas, parsedManifest.canvases.find(c => c.id === canvas.uri))}
+              </li>
+            ))}
+          </ul>
+        </>
       ) : (
         <ul>
           {filtered.slice(0, 20).map(canvas => (
--- src/pages/images/IIIFManifestGrid/IIIFRangeItem.tsx
+++ src/pages/images/IIIFManifestGrid/IIIFRangeItem.tsx
@@ -1,19 +1,49 @@
 import { CozyRange } from 'cozy-iiif';
+import { FolderIcon } from '@/components/FolderIcon';
 
 interface IIIFRangeItemProps {
 
   range: CozyRange;
 
-  selected?: boolean;
-
   onOpen(): void;
-
-  onSelect(): void;
 
 }
 
 export const IIIFRangeItem = (props: IIIFRangeItemProps) => {
 
-  return null;
+  return (
+    <div>
+      <div 
+        className="folder-item cursor-pointer relative rounded-md 
+          w-[200px] h-[200px] flex justify-center items-center">
+
+        <button 
+          onClick={props.onOpen}>
+          <FolderIcon 
+            className="scale w-[190px] h-[190px] transition-all drop-shadow-md" />
+        </button>
+      </div>
+
+      <div className="ml-2">
+        <h3
+          className="text-sm max-w-[200px] overflow-hidden text-ellipsis">
+          {props.range.getLabel()}
+        </h3>
+
+        <p className="pt-1 text-xs text-muted-foreground">
+          {/*
+          {images.length === 0 && folders.length === 0 ? 
+              'Empty' : 
+            images.length > 0 && folders.length > 0 ?
+              `${images.length} Image${images.length > 1 ? 's' : ''} · ${folders.length} Subfolder${folders.length > 1 ? 's' : ''}` :
+            images.length > 0 ?
+              `${images.length} Image${images.length > 1 ? 's' : ''}` :
+              `${folders.length} Subfolder${folders.length > 1 ? 's' : ''}`
+          }
+          */}
+        </p>
+      </div>
+    </div>
+  );
 
 }
