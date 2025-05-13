2849e1091b352089b07c8e4053a6731e64bb58b0
Rainer Simon <rainer@rainersimon.io>
2025-05-13 07:49:20 +0200

    Build config fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macros from 'vite-plugin-babel-macros';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(), 
     macros(),
+    tailwindcss(),
     viteStaticCopy({
       targets: [
         {
