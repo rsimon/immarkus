18dd92fd13434ebc4858256001780ace264b8b07
Rainer Simon <rainer@rainersimon.io>
2025-06-05 11:45:17 +0200

    Feature: copy manifest URL to clipboard

--- src/pages/images/HeaderControls/IIIFOpenOtherViewer/IIIFOpenOtherViewer.tsx
+++ src/pages/images/HeaderControls/IIIFOpenOtherViewer/IIIFOpenOtherViewer.tsx
@@ -1,12 +1,15 @@
-import { ChevronDown, Share2 } from 'lucide-react';
+import { MouseEvent, useState } from 'react';
+import { ChevronDown, ClipboardCheck, Share2 } from 'lucide-react';
 import { IIIFManifestResource } from '@/model';
 import { Button } from '@/ui/Button';
 import { 
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger 
 } from '@/ui/DropdownMenu';
+import { IIIFIcon } from '@/components/IIIFIcon';
 
 interface IIIFOpenOtherViewerProps {
 
@@ -15,6 +18,17 @@ interface IIIFOpenOtherViewerProps {
 }
 
 export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
+
+  const [copied, setCopied] = useState(false);
+
+  const onCopyManifestURL = (evt: MouseEvent) => {
+    evt.preventDefault();
+
+    navigator.clipboard.writeText(props.manifest.uri).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 750);
+    });
+  }
 
   return (
     <DropdownMenu>
@@ -29,7 +43,7 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
       <DropdownMenuContent 
         align="start"
         alignOffset={20}>
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={` https://samvera-labs.github.io/clover-iiif/docs/viewer/demo?iiif-content=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
@@ -37,7 +51,7 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
           </a>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={`https://demo.viewer.glycerine.io/viewer?iiif-content=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
@@ -45,7 +59,7 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
           </a>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={`https://liiive.now/?iiif-content=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
@@ -53,7 +67,7 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
           </a>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={`https://projectmirador.org/embed/?iiif-content=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
@@ -61,7 +75,7 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
           </a>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={`https://theseusviewer.org/?iiif-content=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
@@ -69,12 +83,26 @@ export const IIIFOpenOtherViewer = (props: IIIFOpenOtherViewerProps) => {
           </a>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem asChild className="text-xs">
           <a 
             href={`https://uv-v4.netlify.app/#?manifest=${encodeURIComponent(props.manifest.uri)}`}
             target="_blank">
             Universal Viewer
           </a>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem onSelect={evt => evt.preventDefault} asChild>
+          <button 
+            onClick={onCopyManifestURL}
+            className="w-full flex items-center gap-1.5 cursor-pointer text-xs text-muted-foreground">
+            {copied ? (
+              <ClipboardCheck className="size-4 text-green-700" /> 
+            ) : (
+              <IIIFIcon color className="size-4" />
+            )} <span className="mt-[1px]">Copy Manifest URL</span>
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
