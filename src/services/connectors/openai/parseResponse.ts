848b4899475e07394c73a1415c669937ea26490c
Rainer Simon <rainer@rainersimon.io>
2025-06-23 10:40:54 +0200

    Working OpenAI integration

--- src/services/connectors/openai/parseResponse.ts
+++ src/services/connectors/openai/parseResponse.ts
@@ -1,4 +1,5 @@
-import { ImageAnnotation } from '@annotorious/react';
+import { v4 as uuidv4 } from 'uuid';
+import { AnnotationBody, ImageAnnotation, ShapeType } from '@annotorious/react';
 import { PageTransform, Region } from '@/services/Types';
 
 export const parseResponse = (data: any, _: PageTransform, region: Region): ImageAnnotation[] => {
@@ -8,6 +9,30 @@ export const parseResponse = (data: any, _: PageTransform, region: Region): Imag
   const result = JSON.parse(data.output_text);
 
   const { text } = result;
-  
-  return [];
+    
+  const id = uuidv4();
+
+  return [{
+    id,
+    bodies: [{
+      annotation: id,
+      purpose: 'commenting',
+      value: text
+    } as AnnotationBody],
+    target: {
+      annotation: id,
+      selector: {
+        type: ShapeType.RECTANGLE,
+        geometry: {
+          bounds: {
+            minX: region.x,
+            minY: region.y,
+            maxX: region.x + region.w,
+            maxY: region.y + region.h
+          },
+          ...region
+        }
+      }
+    }
+  }];
 }
