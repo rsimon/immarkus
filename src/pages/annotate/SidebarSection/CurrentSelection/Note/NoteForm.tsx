0e85e102400edadf2f6dc53cbc6a68f0a9b43ded
Rainer Simon <rainer@rainersimon.io>
2025-07-04 09:43:26 +0200

    Note field now displays generator, if any

--- src/pages/annotate/SidebarSection/CurrentSelection/Note/NoteForm.tsx
+++ src/pages/annotate/SidebarSection/CurrentSelection/Note/NoteForm.tsx
@@ -1,3 +1,5 @@
+import { ScanText } from 'lucide-react';
+import { User } from '@annotorious/react';
 import { Textarea } from '@/ui/Textarea';
 
 interface NoteProps {
@@ -5,6 +7,8 @@ interface NoteProps {
   autoFocus?: boolean;
 
   id: string;
+
+  creator?: User; 
 
   value: string;
 
@@ -14,9 +18,18 @@ interface NoteProps {
 
 export const Note = (props: NoteProps) => {
 
+  const { creator } = props;
+
+  const hasGenerator = creator?.name && 'type' in creator;
+
   return (
-    <div className="mt-4">
-      <Textarea 
+    <div className="mt-3">
+      {hasGenerator && (
+        <div className="text-[11px] flex gap-1 items-center justify-end mb-1.5 mr-0.5 text-muted-foreground/80">
+          {creator.name} <ScanText className="size-3.5 mb-[1px]" /> 
+        </div>
+      )}
+      <Textarea
         autoFocus={props.autoFocus}
         id={props.id}
         rows={4}
