bd56dd6973fa24512c62b47f27e40966ee6077f3
Rainer Simon <rainer@rainersimon.io>
2025-08-07 10:10:13 +0200

    HF Inference Providers service: replaced querying with official HF SDK client

--- src/services/connectors/huggingface-inference/submit.ts
+++ src/services/connectors/huggingface-inference/submit.ts
@@ -1,11 +1,13 @@
-import { submitOpenAICompatible } from '@/services/utils';
+import { InferenceClient } from '@huggingface/inference';
+import { fileToBase64, urlToBase64 } from '@/services/utils';
+import { ServiceConnectorResponse } from '@/services/Types';
 
 export const submit = (image: File | string, options?: Record<string, any>) => {
-  const apiKey = options['access-token'];
+  const hfToken = options['access-token'];
   const model = options['model'];
 
   // Should never happen
-  if (!apiKey || !model)
+  if (!hfToken || !model)
     throw new Error('Missing access configuration');
 
   const generator = {
@@ -14,15 +16,32 @@ export const submit = (image: File | string, options?: Record<string, any>) => {
     homepage: 'https://huggingface.co/docs/inference-providers'
   };
 
-  return submitOpenAICompatible(
-    image,
-    apiKey,
-    'https://router.huggingface.co/v1',
-    model,
-    generator, 
-    { 
-      'anthropic-dangerous-direct-browser-access': 'true' 
-    }
-  );
+  const client = new InferenceClient(hfToken);
 
+  const submit = (imageUrl: string) =>
+    client.chatCompletion({
+      model,
+      messages: [
+        {
+          role: 'user',
+          content: [{
+            type: 'text',
+            text: 'Extract all text from this image. Your response must be ONLY valid JSON in this format: { "text": "all extracted text goes here" } Preserve whitespace and newline formatting in the text output.'
+          },{
+            type: 'image_url',
+            image_url: {
+              url: imageUrl
+            }
+          }]
+        },
+      ],
+    }).then((data: any) => ({ generator, data } as ServiceConnectorResponse));
+
+  if (typeof image === 'string') {
+    return urlToBase64(image).then(base64 =>  
+      submit(`data:image/jpeg;base64,${base64}`));
+  } else {
+    return fileToBase64(image as File).then(base64 => 
+      submit(`data:image/jpeg;base64,${base64}`));
+  }
 }
