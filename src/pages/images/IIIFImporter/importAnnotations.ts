2767060987a33fcee0d7b2767645422e6e02e424
Rainer Simon <rainer@rainersimon.io>
2025-09-26 12:07:19 +0200

    Annotation body crosswalk

--- src/pages/images/IIIFImporter/importAnnotations.ts
+++ src/pages/images/IIIFImporter/importAnnotations.ts
@@ -1,7 +1,7 @@
 import murmur from 'murmurhash';
 import { CozyCanvas } from 'cozy-iiif';
 import { parseW3CImageAnnotation, serializeW3CImageAnnotation } from '@annotorious/react';
-import type { W3CImageAnnotation } from '@annotorious/react';
+import type { AnnotationBody, W3CImageAnnotation } from '@annotorious/react';
 
 export const validateAnnotations = (canvases: CozyCanvas[]) => {
   const annotations = canvases.reduce<any[]>((all, canvas) => {
@@ -20,6 +20,35 @@ export const validateAnnotations = (canvases: CozyCanvas[]) => {
   return { total: annotations.length, valid: valid.length };
 }
 
+/**
+ * This method will drop all bodies that are not plaintext or HTML format,
+ * and concatenate all remaining to a single comment body.
+ */
+const crosswalkAnnotationBody = (bodies: AnnotationBody[]): AnnotationBody[] => {
+  const crosswalkedValues = bodies.reduce<string[]>((all, body) => {
+    // No body value, or body has a type that's not 'TextualBody'
+    if (!body.value || (body.type && body.type !== 'TextualBody')) return all;
+
+    if (!('format' in body) || body.format === 'text/plain') {
+      // text/plain or no format - consider plaintext
+      return [...all, body.value.toString()];
+    } else if (body.format === 'text/html') {
+      // Strip HTML
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(body.value, 'text/html');
+      return [...all, doc.body.innerText];
+    } else {
+      return all;
+    }
+  }, []);
+
+  return [{
+    type: 'TextualBody',
+    purpose: 'commenting',
+    value: crosswalkedValues.join('\n---\n')
+  } as AnnotationBody]
+}
+
 export const crosswalkAnnotations = (manifestId: string, canvases: CozyCanvas[]): W3CImageAnnotation[] =>
   canvases.reduce<W3CImageAnnotation[]>((all, canvas) => {
     if ((canvas.annotations || []).length > 0) {
@@ -32,7 +61,12 @@ export const crosswalkAnnotations = (manifestId: string, canvases: CozyCanvas[])
           // Normalize annotations by "double"-crosswalking them
           const parsed = parseW3CImageAnnotation(orig as W3CImageAnnotation).parsed;
           if (parsed) {
-            const normalized = serializeW3CImageAnnotation(parsed, `iiif:${manifestId}:${canvasId}`);
+            const crosswalked = {
+              ...parsed,
+              bodies: crosswalkAnnotationBody(parsed.bodies)
+            };
+
+            const normalized = serializeW3CImageAnnotation(crosswalked, `iiif:${manifestId}:${canvasId}`);
             return [...all, normalized];
           } else {
             return all;
