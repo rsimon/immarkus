3aeabaeed304847cea49166094d8ad9acbe59e3d
Rainer Simon <rainer@rainersimon.io>
2026-03-04 11:55:30 +0100

    Selected annotations now have thicker stroke

--- src/pages/annotate/WorkspaceSection/useDrawingStyles.ts
+++ src/pages/annotate/WorkspaceSection/useDrawingStyles.ts
@@ -1,10 +1,18 @@
-import { DataModel, useDataModel } from '@/store';
 import chroma from 'chroma-js';
-import { Color, DrawingStyle, ImageAnnotation, W3CAnnotationBody } from '@annotorious/react';
+import { DataModel, useDataModel } from '@/store';
+import type { 
+  AnnotationState, 
+  Color, 
+  DrawingStyle, 
+  ImageAnnotation, 
+  W3CAnnotationBody 
+} from '@annotorious/react';
 
 export const colorByEntityType = (
   model: DataModel
-) => (annotation: ImageAnnotation): DrawingStyle => {
+) => (annotation: ImageAnnotation, state: AnnotationState): DrawingStyle => {
+
+  const strokeWidth = state?.selected ? 3 : 1.2;
 
   const firstEntityBody =
     annotation.bodies.find(b => b.purpose === 'classifying') as unknown as W3CAnnotationBody;
@@ -16,19 +24,19 @@ export const colorByEntityType = (
       fill: entityType.color as Color,
       stroke: chroma(entityType.color).darken(2).hex() as Color,
       strokeOpacity: 1,
-      strokeWidth: 1.2
+      strokeWidth
     } : {
       fill: '#000000',
       stroke: '#000000',
       strokeOpacity: 1,
-      strokeWidth: 1.2
+      strokeWidth
     };
   } else {
     return {
       fill: '#000000',
       stroke: '#000000',
       strokeOpacity: 1,
-      strokeWidth: 1.2
+      strokeWidth
     }
   }
 
