ed8738a6310c6d84a122a67c1c24c68cdf9cbeb3
Rainer Simon <rainer@rainersimon.io>
2026-04-15 12:48:31 +0200

    Starting to spec out the (pretty big...) image search dialog

--- src/pages/annotate/SmartTools/VisualSearch/ImageSearchDialog.tsx
+++ src/pages/annotate/SmartTools/VisualSearch/ImageSearchDialog.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
+import type { ImageAnnotation } from '@annotorious/react';
+import { useSelection } from '@annotorious/react-manifold';
 import { LoadedImage } from '@/model';
 import { useVisualSearch } from '@/pages/visualsearch/useVisualSearch';
-import { Dialog, DialogContent } from '@/ui/Dialog';
+import { Dialog, DialogContent, DialogTitle } from '@/ui/Dialog';
+import { FileImageSnippet, getImageSnippet } from '@/utils/getImageSnippet';
 
 interface ImageSearchDialogProps {
 
@@ -14,6 +18,8 @@ interface ImageSearchDialogProps {
 
 export const ImageSearchDialog = (props: ImageSearchDialogProps) => {
 
+  const { selected } = useSelection();
+
   const vs = useVisualSearch();
 
   const onOpenChange = (open: boolean) => {
@@ -21,27 +27,30 @@ export const ImageSearchDialog = (props: ImageSearchDialogProps) => {
       props.onClose();
   }
 
-  /*
-  const onSearchImages = () => {
-      const { annotation, annotatorId } = selected[0];
-  
-      const image = props.images.find(i => i.id === annotatorId);
-  
-      console.log('running similarity search');
-  
-      getImageSnippet(
-        image, 
-        annotation as ImageAnnotation, 
-        true, // Download IIIF
-        'png'
-      ).then((snippet: FileImageSnippet) => {
-        const blob = new Blob([snippet.data as BlobPart], { type: 'image/png' });
-        vs.index.query(blob as File).then(result => {
-          console.log(result);
-        })
-      });
-    }
-  */
+  useEffect(() => {
+    if (!props.open || selected.length !== 1) return;
+
+    const { annotation, annotatorId } = selected[0]; 
+
+    const image = props.images.find(i => i.id === annotatorId);
+
+    if (!image) return;
+
+    getImageSnippet(
+      image, 
+      annotation as ImageAnnotation, 
+      true, // if IIIF -> download
+      'png'
+    ).then((snippet: FileImageSnippet) => {
+      console.debug('Running similarity search');
+
+      const blob = new Blob([snippet.data as BlobPart], { type: 'image/png' });
+
+      vs.index.query(blob).then(result => {
+        console.log(result);
+      })
+    });
+  }, [props.open, vs, selected, props.images]);
 
   return (
     <Dialog
@@ -50,6 +59,9 @@ export const ImageSearchDialog = (props: ImageSearchDialogProps) => {
       <DialogContent 
         className="rounded-lg flex flex-col md:w-auto h-11/12 max-w-11/12 p-0 
           overflow-hidden relative gap-1">
+        <DialogTitle>
+          Search Results
+        </DialogTitle>
 
         Hello World
         
