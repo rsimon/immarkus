f8e20abf50007a8396e4748f72700bf0f363757c
Rainer Simon <rainer@rainersimon.io>
2026-05-22 12:55:53 +0200

    Hover card previews for 'Add Images' dialog

--- src/pages/annotate/HeaderSection/AddImage/AddImageListItems/ImageListItem.tsx
+++ src/pages/annotate/HeaderSection/AddImage/AddImageListItems/ImageListItem.tsx
@@ -2,6 +2,7 @@ import { Check, MessagesSquare } from 'lucide-react';
 import { IIIFIcon } from '@/components/IIIFIcon';
 import { Thumbnail } from '@/components/Thumbnail';
 import { CanvasInformation, FileImage } from '@/model';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/ui/HoverCard';
 
 interface ImageListItemProps {
 
@@ -28,16 +29,28 @@ export const ImageListItem = (props: ImageListItemProps) => {
         disabled={isOpen}
         className="flex gap-3 w-full relative"
         onClick={onSelect}>
-        {isOpen ? (
-          <div className="relative">
-            <Thumbnail image={image} /> 
-            <div className="absolute w-full h-full top-0 left-0 bg-white/60 flex items-center justify-center">
-              <Check className="text-black h-10 w-10" />
-            </div>
-          </div>
-        ) : (
-          <Thumbnail image={image} /> 
-        )}
+        <div className="relative">
+          <HoverCard>
+            <HoverCardTrigger>
+              <Thumbnail image={image} /> 
+              {isOpen && (
+                <div className="absolute w-full h-full top-0 left-0 bg-white/60 flex items-center justify-center">
+                  <Check className="text-black h-10 w-10" />
+                </div>
+              )}
+            </HoverCardTrigger>
+
+            <HoverCardContent 
+              sideOffset={8}
+              className="z-999 p-0 border border-white shadow-xl overflow-hidden min-w-0 w-auto"
+              collisionPadding={20}>
+              <Thumbnail 
+                image={image} 
+                size={600}
+                className="h-auto w-auto aspect-auto max-w-100 max-h-100 border-0"/>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
 
         {isIIIF && (
           <IIIFIcon
@@ -47,7 +60,7 @@ export const ImageListItem = (props: ImageListItemProps) => {
         
         <div className="grow line-clamp-3 overflow-hidden text-ellipsis text-left">
           <div className="py-1 space-y-1">
-            <div>{image.name}</div>
+            <div className="whitespace-nowrap truncate">{image.name}</div>
             {props.annotations > 0 && (
               <div className="flex gap-1 items-center text-muted-foreground">
                 <MessagesSquare className="size-3.5" /> {props.annotations}
