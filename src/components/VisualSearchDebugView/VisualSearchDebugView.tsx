7ec7860ff34792e12b0ed7dc2626303d1beccfa2
Rainer Simon <rainer@rainersimon.io>
2026-05-27 15:51:18 +0200

    Added debug utility for inspecting indexed segments

---
+++ src/components/VisualSearchDebugView/VisualSearchDebugView.tsx
@@ -0,0 +1,141 @@
+import { useEffect, useMemo } from 'react';
+import type { IndexedImageSegment } from 'browser-visual-search';
+import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer, useAnnotator } from '@annotorious/react';
+import type { AnnotationState, DrawingStyleExpression, ImageAnnotation } from '@annotorious/react';
+import { LoadedImage } from '@/model';
+import { Dialog, DialogContent, DialogTitle } from '@/ui/Dialog';
+import { getOSDTilesets } from '@/utils/iiif';
+import { useVisualSearch } from '@/utils/useVisualSearch';
+import { boundsToAnnotation } from '@/utils/getImageSnippetHelpers';
+
+export interface VisualSearchDebugDialogProps {
+
+  image: LoadedImage;
+
+  open: boolean;
+
+  onOpenChange(open: boolean): void;
+
+}
+
+interface VisualSearchDebugViewerProps {
+
+  image: LoadedImage;
+
+}
+
+export const getBounds = (segment: IndexedImageSegment, image: LoadedImage) => {
+  if ('canvas' in image) {
+    const { width, height} = image.canvas;
+    const [ nx, ny, nw, nh ] = segment.normalizedBounds;
+
+    const x = nx * width;
+    const y = ny * height;
+    const w = nw * width;
+    const h = nh * height;
+
+    return [x, y, w, h];
+  } else {
+    return segment.pxBounds;
+  }
+}
+
+const VisualSearchDebugViewer = (props: VisualSearchDebugViewerProps) => {
+  const vs = useVisualSearch();
+
+  const anno = useAnnotator();
+
+  const options: OpenSeadragon.Options = useMemo(() => ({
+    tileSources: 'data' in props.image ? {
+      type: 'image',
+      url: URL.createObjectURL(props.image.data)
+    } as object : getOSDTilesets(props.image.canvas),
+    gestureSettingsMouse: {
+      clickToZoom: false,
+      dblClickToZoom: false
+    },
+    crossOriginPolicy: 'Anonymous',
+    showNavigationControl: false,
+    minZoomLevel: 0.1,
+    maxZoomLevel: 100
+  }), [props.image]);
+
+  const style: DrawingStyleExpression = useMemo(() => (_: ImageAnnotation, state: AnnotationState) => {
+    return {
+      fill: '#ff1493',
+      fillOpacity: state?.hovered ? 0.2 : 0.02,
+      stroke: '#ff1493',
+      strokeOpacity: state?.hovered ? 0.7 : 0.2
+    };
+  }, []);
+
+  const annotations = useMemo(() => {
+    if (!vs.index) return;
+    const image = vs.index.getImage(props.image.id);
+    return image?.segments.map(segment => {
+      const [x, y, w, h] = getBounds(segment, props.image);
+      return boundsToAnnotation({
+        minX: x, 
+        minY: y,
+        maxX: x + w,
+        maxY: y + h
+      });
+    })
+  }, [vs, props.image]);
+
+  useEffect(() => {
+    if (!vs.index || !anno) return;
+
+    const image = vs.index.getImage(props.image.id);
+    if (!image) return;
+
+    const annotatios = image?.segments.map(segment => {
+      const [x, y, w, h] = getBounds(segment, props.image);
+      return boundsToAnnotation({
+        minX: x, 
+        minY: y,
+        maxX: x + w,
+        maxY: y + h
+      });
+    });
+
+    anno.setAnnotations(annotations);
+
+    return () => {
+      anno.clearAnnotations();
+    }
+  }, [vs, anno, props.image]);
+
+  return (
+    <OpenSeadragonAnnotator
+      style={style}>
+      <OpenSeadragonViewer
+        options={options} 
+        className="size-full bg-muted border rounded" />
+    </OpenSeadragonAnnotator>
+  )
+
+}
+
+export const VisualSearchDebugDialog = (props: VisualSearchDebugDialogProps) => {
+
+  return (
+    <Dialog
+      open={props.open}
+      onOpenChange={props.onOpenChange}>
+      <DialogContent className="h-11/12 w-11/12 max-w-11/12 flex flex-col">
+        <DialogTitle>
+          {props.image.name}
+        </DialogTitle>
+
+        <div className="grow relative">
+          <Annotorious>
+            <VisualSearchDebugViewer
+              image={props.image} />
+          </Annotorious>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+
+}
--- src/pages/images/ItemOverview/ItemGrid/ImageItem/ImageItemActions.tsx
+++ src/pages/images/ItemOverview/ItemGrid/ImageItem/ImageItemActions.tsx
@@ -1,5 +1,8 @@
-import { Image } from '@/model';
-import { ImageIcon, Images, MoreVertical, NotebookPen } from 'lucide-react';
+import { useState } from 'react';
+import { Bug, ImageIcon, Images, MoreVertical, NotebookPen } from 'lucide-react';
+import { VisualSearchDebugDialog } from '@/components/VisualSearchDebugView';
+import { LoadedImage } from '@/model';
+import { useVisualSearchAvailable } from '@/pages/annotate/SmartTools/VisualSearch';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,7 +14,7 @@ interface ImageItemActionProps {
 
   className?: string;
 
-  image: Image;
+  image: LoadedImage;
 
   onSelect(): void;
 
@@ -23,29 +26,53 @@ interface ImageItemActionProps {
 
 export const ImageItemActions = (props: ImageItemActionProps) => {
 
+  const hasVisualSearch = useVisualSearchAvailable();
+
+  const [showVisualSearchDebug, setShowVisualSearchDebug] = useState(false);
+
+  const onOpenVSDebug = (e: Event) => {
+    e.preventDefault();
+    setShowVisualSearchDebug(true);
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button
-          className="item-actions-trigger absolute bottom-2 right-1">
-          <MoreVertical size={18} />
-        </button>
-      </DropdownMenuTrigger>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button
+            className="item-actions-trigger absolute bottom-2 right-1">
+            <MoreVertical size={18} />
+          </button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="start">
-        <DropdownMenuItem onSelect={props.onSelect}>
-          <NotebookPen className="size-4 text-muted-foreground mr-2" /> Metadata
-        </DropdownMenuItem>
+        <DropdownMenuContent align="start">
+          <DropdownMenuItem onSelect={props.onSelect}>
+            <NotebookPen className="size-4 text-muted-foreground mr-2" /> Metadata
+          </DropdownMenuItem>
 
-        <DropdownMenuItem onSelect={props.onOpen}>
-          <ImageIcon className="size-4 text-muted-foreground mr-2" /> Open image
-        </DropdownMenuItem>
+          <DropdownMenuItem onSelect={props.onOpen}>
+            <ImageIcon className="size-4 text-muted-foreground mr-2" /> Open image
+          </DropdownMenuItem>
 
-        <DropdownMenuItem onSelect={props.onAddToWorkspace}>
-          <Images className="size-4 text-muted-foreground mr-2" /> Add to workspace
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem onSelect={props.onAddToWorkspace}>
+            <Images className="size-4 text-muted-foreground mr-2" /> Add to workspace
+          </DropdownMenuItem>
+
+          {hasVisualSearch && (
+            <DropdownMenuItem onSelect={onOpenVSDebug}>
+              <Bug className="size-4 text-muted-foreground mr-2" /> Inspect visual search segments
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {hasVisualSearch && (
+        <VisualSearchDebugDialog 
+          image={props.image}
+          open={showVisualSearchDebug}
+          onOpenChange={setShowVisualSearchDebug} />
+      )}
+    </>
   )
 
 }
