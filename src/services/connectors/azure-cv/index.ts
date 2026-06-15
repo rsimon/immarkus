cd42b3490ddd837d68bbef6a7dbb2a3b9e3068b5
Rainer Simon <rainer@rainersimon.io>
2026-06-15 07:43:03 +0200

    Dependencies + removed Azure connector

--- src/services/connectors/azure-cv/index.ts
+++
@@ -1,5 +0,0 @@
-import { transcribe } from './transcribe';
-import { parseResponse as parseTranscriptionResponse } from './parseResponse';
-import { ServiceConnector } from '@/services/Types';
-
-export default { transcribe, parseTranscriptionResponse } as ServiceConnector;
