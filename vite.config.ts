887820b37615f14176d37de9ca05a2320d38c0c5
Rainer Simon <rainer@rainersimon.io>
2026-08-05 07:42:44 +0200

    Dependency updates + fixed Vite config deprecation warning

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
       '@gradio/client': path.resolve('./node_modules/@gradio/client/dist/index.js')
     }
   },
